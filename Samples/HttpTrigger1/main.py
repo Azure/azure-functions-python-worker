8e8f2ce72c1b1a132c29362a088a752e638206dc
Asavari Tayal <asavaritayal@Asavaris-MacBook-Pro.local>
2018-01-18 10:32:26 -0800

    Adding a samples directory

---
+++ Samples/HttpTrigger1/main.py
@@ -0,0 +1,14 @@
+def main(context,req,res):
+
+    context.logger.info('Python HTTP trigger function processed a request.')
+
+    if req.params and req.params['name']:
+        res.content = 'Hello ' + req.params['name']
+
+    elif req.data and req.data['name']:
+        res.content = 'Hello ' + req.data['name']
+        
+    else:
+        res.content = 'Please pass a name on the query string or in the request body.'
+        res.status_code = 400
+  

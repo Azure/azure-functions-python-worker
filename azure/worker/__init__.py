44e5f2dd425eb6cbba18b768940af2907fd48ef6
Yury Selivanov <yury@magic.io>
2018-02-16 17:43:08 -0500

    Make everything async

--- azure/worker/__init__.py
+++ azure/worker/__init__.py
@@ -1,32 +1,14 @@
-import queue
-
-import grpc
-
-try:
-    from . import protos
-except ImportError:
-    raise RuntimeError(
-        'cannot start worker; generate pb2/grpc files in azure/worker/protos')
-
+from . import aio_compat
 from . import dispatcher
 
 
+async def start_async(host, port, worker_id, request_id):
+    disp = await dispatcher.Dispatcher.connect(
+        host, port, worker_id, request_id,
+        connect_timeout=5.0)
+
+    await disp.dispatch_forever()
+
+
 def start(host: str, port: int, worker_id: str, request_id: str):
-    channel = grpc.insecure_channel(f'{host}:{port}')
-    stub = protos.FunctionRpcStub(channel)
-
-    def gen(q):
-        while True:
-            yield q.get()
-
-    response_queue = queue.Queue()
-    response_queue.put(protos.StreamingMessage(
-        request_id=request_id,
-        start_stream=protos.StartStream(
-            worker_id=worker_id)))
-
-    resp_stream = stub.EventStream(gen(response_queue))
-    disp = dispatcher.Dispatcher(response_queue, request_id)
-
-    for resp in resp_stream:
-        disp.dispatch(resp)
+    return aio_compat.run(start_async(host, port, worker_id, request_id))

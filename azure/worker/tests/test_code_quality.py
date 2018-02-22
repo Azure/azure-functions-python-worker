cacafe35b34208e9b450db451fdef6eaacd438a9
Elvis Pranskevichus <elvis@magic.io>
2018-02-22 14:33:09 -0500

    Add flake8 linting to the test suite

--- azure/worker/tests/test_code_quality.py
+++ azure/worker/tests/test_code_quality.py
@@ -26,3 +26,25 @@ class TestCodeQuality(unittest.TestCase):
             output = ex.output.decode()
             raise AssertionError(
                 f'mypy validation failed:\n{output}') from None
+
+    def test_flake8(self):
+        try:
+            import flake8  # NoQA
+        except ImportError:
+            raise unittest.SkipTest('flake8 moudule is missing')
+
+        config_path = os.path.join(ROOT_PATH, '.flake8')
+        if not os.path.exists(config_path):
+            raise unittest.SkipTest('could not locate the .flake8 file')
+
+        try:
+            subprocess.run(
+                [sys.executable, '-m', 'flake8', '--config', config_path],
+                check=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                cwd=ROOT_PATH)
+        except subprocess.CalledProcessError as ex:
+            output = ex.output.decode()
+            raise AssertionError(
+                f'flake8 validation failed:\n{output}') from None

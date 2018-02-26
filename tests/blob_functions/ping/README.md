f61c5e794426bbaccba021a2a3815ec2e5e07bf2
Elvis Pranskevichus <elvis@magic.io>
2018-02-26 16:27:34 -0500

    Initial support for Blob binding
    Input bindings are always passed as an `InputStream` object which
    implements `BufferedIOBase` and is a read-only non-seekable stream.
    
    Output parameters can be strings, bytes, or file-like objects.

---
+++ tests/blob_functions/ping/README.md
@@ -0,0 +1,2 @@
+This function is used to check the host availability in tests.
+Please do not remove.
---
+++ tests/test_blob_functions.py
@@ -0,0 +1,44 @@
+from azure.worker import testutils
+
+
+class TestBlobFunctions(testutils.WebHostTestCase):
+
+    @classmethod
+    def get_script_dir(cls):
+        return 'blob_functions'
+
+    def test_blob_io_str(self):
+        r = self.webhost.request('POST', 'put_blob_str', data='test-data')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.text, 'OK')
+
+        r = self.webhost.request('GET', 'get_blob_str', params={'file': ''})
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.text, 'test-data')
+
+    def test_blob_io_bytes(self):
+        r = self.webhost.request('POST', 'put_blob_bytes',
+                                 data='test-dată'.encode('utf-8'))
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.text, 'OK')
+
+        r = self.webhost.request('POST', 'get_blob_bytes')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.text, 'test-dată')
+
+    def test_blob_io_filelike(self):
+        r = self.webhost.request('POST', 'put_blob_filelike')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.text, 'OK')
+
+        r = self.webhost.request('POST', 'get_blob_filelike')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.text, 'filelike')
+
+    def test_blob_io_return(self):
+        r = self.webhost.request('POST', 'put_blob_return')
+        self.assertEqual(r.status_code, 200)
+
+        r = self.webhost.request('POST', 'get_blob_return')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.text, 'FROM RETURN')

fa36e1ef05f2b3574933f67e2a99f1762dca485a
Elvis Pranskevichus <elvis@magic.io>
2018-04-26 16:08:04 -0400

    Add support for grpcMaxMessageLength command line argument
    Fixes: #109

--- azure/worker/__init__.py
+++ azure/worker/__init__.py
@@ -2,13 +2,15 @@ from . import aio_compat
 from . import dispatcher
 
 
-async def start_async(host, port, worker_id, request_id):
+async def start_async(host, port, worker_id, request_id, grpc_max_msg_len):
     disp = await dispatcher.Dispatcher.connect(
         host, port, worker_id, request_id,
-        connect_timeout=5.0)
+        connect_timeout=5.0, max_msg_len=grpc_max_msg_len)
 
     await disp.dispatch_forever()
 
 
-def start(host: str, port: int, worker_id: str, request_id: str):
-    return aio_compat.run(start_async(host, port, worker_id, request_id))
+def start(host: str, port: int, worker_id: str, request_id: str,
+          grpc_max_msg_len: int):
+    return aio_compat.run(start_async(
+        host, port, worker_id, request_id, grpc_max_msg_len))

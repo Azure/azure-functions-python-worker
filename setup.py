77a07bb8fe2af1ce9612c9118b25c86aed890c19
Elvis Pranskevichus <elvis@magic.io>
2018-08-31 15:27:06 -0400

    Bump Host version

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ from setuptools.command import develop
 
 
 # TODO: change this to something more stable when available.
-WEBHOST_URL = ('https://ci.appveyor.com/api/buildjobs/k11e6ngq22n1kgfm'
-               '/artifacts/Functions.Binaries.2.0.12041-alpha.no-runtime.zip')
+WEBHOST_URL = ('https://ci.appveyor.com/api/buildjobs/ykwva3bprej48cfa'
+               '/artifacts/Functions.Binaries.2.0.12050-alpha.no-runtime.zip')
 
 # Extensions necessary for non-core bindings.
 AZURE_EXTENSIONS = [

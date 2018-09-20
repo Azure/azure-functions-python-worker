0703cd46f4054cb8b7d09a58e40e3a026e1de82b
pragnagopa <pgopa@microsoft.com>
2018-09-20 13:52:46 -0700

    Update to latest extension packages

--- setup.py
+++ setup.py
@@ -22,27 +22,27 @@ WEBHOST_URL = ('https://ci.appveyor.com/api/buildjobs/f5qsvh40oh8ny47c'
 AZURE_EXTENSIONS = [
     {
         "id": "Microsoft.Azure.WebJobs.Script.ExtensionsMetadataGenerator",
-        "version": "1.0.0"
+        "version": "1.0.1"
     },
     {
         "id": "Microsoft.Azure.WebJobs.Extensions.CosmosDB",
-        "version": "3.0.1-beta1"
+        "version": "3.0.1"
     },
     {
         "id": "Microsoft.Azure.WebJobs.Extensions.EventHubs",
-        "version": "3.0.0-beta8"
+        "version": "3.0.0"
     },
     {
         "id": "Microsoft.Azure.WebJobs.Extensions.EventGrid",
-        "version": "2.0.0-beta3"
+        "version": "2.0.0"
     },
     {
         "id": "Microsoft.Azure.WebJobs.Extensions.Storage",
-        "version": "3.0.0-beta8"
+        "version": "3.0.0"
     },
     {
-        "id": "Microsoft.Azure.WebJobs.ServiceBus",
-        "version": "3.0.0-beta8"
+        "id": "Microsoft.Azure.WebJobs.Extensions.ServiceBus",
+        "version": "3.0.0"
     },
 ]
 

513afdc329d510765ab392be1743bc031e73e7a4
Elvis Pranskevichus <elvis@magic.io>
2018-11-15 16:39:34 -0500

    Fix split logging

--- azure/functions_worker/logging.py
+++ azure/functions_worker/logging.py
@@ -4,17 +4,18 @@ import sys
 
 
 logger = logging.getLogger('azure.functions_worker')
-error_logger = logger
+error_logger = logging.getLogger('azure.functions_worker_errors')
 
 
 def setup(log_level, log_destination):
-    global error_logger
-
     if log_level == 'TRACE':
         log_level = 'DEBUG'
 
     formatter = logging.Formatter(
         'LanguageWorkerConsoleLog %(levelname)s: %(message)s')
+
+    error_handler = None
+    handler = None
 
     if log_destination is None:
         # With no explicit log destination we do split logging,
@@ -22,9 +23,6 @@ def setup(log_level, log_destination):
         error_handler = logging.StreamHandler(sys.stderr)
         error_handler.setFormatter(formatter)
         error_handler.setLevel(getattr(logging, log_level))
-
-        error_logger = logging.getLogger('azure.functions_worker_errors')
-        error_logger.addHandler(error_handler)
 
         handler = logging.StreamHandler(sys.stdout)
 
@@ -37,8 +35,14 @@ def setup(log_level, log_destination):
     else:
         handler = logging.FileHandler(log_destination)
 
+    if error_handler is None:
+        error_handler = handler
+
     handler.setFormatter(formatter)
     handler.setLevel(getattr(logging, log_level))
 
     logger.addHandler(handler)
     logger.setLevel(getattr(logging, log_level))
+
+    error_logger.addHandler(error_handler)
+    error_logger.setLevel(getattr(logging, log_level))

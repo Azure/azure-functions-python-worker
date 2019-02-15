4beea9076f269bcdb6d952bf6e715310233d308a
Maheer Iqbal <42051041+maiqbal11@users.noreply.github.com>
2019-02-15 12:56:52 -0800

    Generate platform specific run environments

--- python/run_worker.py
+++
@@ -1,8 +0,0 @@
-import platform
-import subprocess
-
-if __name__ == '__main__':
-
-    subprocess.run(['python', '-m', 'pip', 'install', '.'])
-    subprocess.run(['python', 'setup.py', 'build'])
-    subprocess.run(['python', '-m', 'azure.functions_worker'])

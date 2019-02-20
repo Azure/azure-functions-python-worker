8e924787af40e052dad2bceb16666c04a9b2db22
Elvis Pranskevichus <elvis@magic.io>
2019-02-20 13:14:40 -0500

    Fix handling of multiple Out params (#317)
    Fixes: #296

---
+++ tests/queue_functions/put_queue_multiple_out/main.py
@@ -0,0 +1,8 @@
+import azure.functions as func
+
+
+def main(req: func.HttpRequest, resp: func.Out[func.HttpResponse],
+         msg: func.Out[func.QueueMessage]) -> None:
+    data = req.get_body().decode()
+    msg.set(func.QueueMessage(body=data))
+    resp.set(func.HttpResponse(body='HTTP response: {}'.format(data)))

59d34dee3c26802a366b8094d51d953e69852f3d
Elvis Pranskevichus <elvis@magic.io>
2019-05-10 11:20:22 -0400

    Add support for untyped queue trigger binding

--- azure/functions_worker/bindings/queue.py
+++ azure/functions_worker/bindings/queue.py
@@ -60,6 +60,10 @@ class QueueMessageInConverter(meta.InConverter,
             cls, pytype: type, datatype: protos.BindingInfo.DataType) -> bool:
         if datatype is protos.BindingInfo.undefined:
             return issubclass(pytype, azf_abc.QueueMessage)
+        elif (datatype is protos.BindingInfo.binary):
+            return issubclass(pytype, bytes)
+        elif (datatype is protos.BindingInfo.string):
+            return issubclass(pytype, str)
         else:
             return False
 
@@ -68,6 +72,31 @@ class QueueMessageInConverter(meta.InConverter,
                    pytype: typing.Optional[type],
                    trigger_metadata) -> typing.Any:
         data_type = data.WhichOneof('data')
+
+        if pytype is str:
+            # Bound as dataType: string
+            if data_type == 'string':
+                return data.string
+            else:
+                raise ValueError(
+                    f'unexpected type of data received for the "queue" binding'
+                    f' declared to receive a string: {data_type!r}'
+                )
+
+            return data.string
+
+        elif pytype is bytes:
+            if data_type == 'bytes':
+                return data.bytes
+            elif data_type == 'string':
+                # This should not happen with the correct dataType spec,
+                # but we can be forgiving in this case.
+                return data.string.encode('utf-8')
+            else:
+                raise ValueError(
+                    f'unexpected type of data received for the "queue" binding'
+                    f'declared to receive bytes: {data_type!r}'
+                )
 
         if data_type == 'string':
             body = data.string
---
+++ tests/queue_functions/queue_trigger_untyped/main.py
@@ -0,0 +1,8 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def main(msg: str) -> str:
+    return msg

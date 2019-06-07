5719d789f456f660d085cfd1c5a35a7f92b0e30c
Maheer Iqbal <42051041+maiqbal11@users.noreply.github.com>
2019-06-07 08:03:39 -0700

    raise unittest

--- tests/test_code_quality.py
+++ tests/test_code_quality.py
@@ -27,7 +27,7 @@ class TestCodeQuality(unittest.TestCase):
                 cwd=str(ROOT_PATH))
         except subprocess.CalledProcessError as ex:
             if (sys.version_info[1] == 7 and sys.version_info[2] == 3):
-                unittest.SkipTest("Subprocess start failing for ver 3.7.3")
+                raise unittest.SkipTest("Subprocess start failing for ver 3.7.3")
             print("MyPy test failed")
             output = ex.output.decode()
             print(ex.output)

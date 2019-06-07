522845677533134b0bfea0270f1fa97dfa2be17a
Maheer Iqbal <42051041+maiqbal11@users.noreply.github.com>
2019-06-07 07:42:24 -0700

    Execute test without try

--- tests/test_code_quality.py
+++ tests/test_code_quality.py
@@ -17,20 +17,20 @@ class TestCodeQuality(unittest.TestCase):
             print("MyPy is missing")
             raise unittest.SkipTest('mypy module is missing')
 
-        try:
-            print("Running test")
-            subprocess.run(
-                [sys.executable, '-m', 'mypy', '-m', 'azure.functions_worker'],
-                check=True,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                cwd=str(ROOT_PATH))
-        except subprocess.CalledProcessError as ex:
-            print("MyPy test failed")
-            output = ex.output.decode()
-            print(ex.output)
-            raise AssertionError(
-                f'mypy validation failed:\n{output}') from None
+        # try:
+        print("Running test")
+        subprocess.run(
+            [sys.executable, '-m', 'mypy', '-m', 'azure.functions_worker'],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            cwd=str(ROOT_PATH))
+        # except subprocess.CalledProcessError as ex:
+        #     print("MyPy test failed")
+        #     output = ex.output.decode()
+        #     print(ex.output)
+        #     raise AssertionError(
+        #         f'mypy validation failed:\n{output}') from None
 
     # def test_flake8(self):
     #     try:

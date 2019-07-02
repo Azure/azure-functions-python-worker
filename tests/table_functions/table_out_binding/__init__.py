b805b9be7466c840a4019399714938de42bbb327
Maheer Iqbal <42051041+maiqbal11@users.noreply.github.com>
2019-07-02 14:50:58 -0700

    Allow json data type for generic table binding (#475)

---
+++ tests/table_functions/table_out_binding/__init__.py
@@ -0,0 +1,13 @@
+import json
+import uuid
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest, resp: func.Out[func.HttpResponse]):
+    row_key_uuid = str(uuid.uuid4())
+    table_dict = {'PartitionKey': 'test', 'RowKey': row_key_uuid}
+    table_json = json.dumps(table_dict)
+    http_resp = func.HttpResponse(status_code=200, headers=table_dict)
+    resp.set(http_resp)
+    return table_json

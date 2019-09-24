4fb4f5e92baefb66df87c15477c038afff5d8658
Maheer Iqbal <42051041+maiqbal11@users.noreply.github.com>
2019-09-24 15:45:24 -0700

    Add start up code to worker

--- azure_functions_worker/constants.py
+++ azure_functions_worker/constants.py
@@ -1,3 +1,10 @@
 # Capabilities
 RAW_HTTP_BODY_BYTES = "RawHttpBodyBytes"
 TYPED_DATA_COLLECTION = "TypedDataCollection"
+
+# User packages
+PKGS_PATH = "site/wwwroot/.python_packages"
+VENV_PKGS_PATH = "site/wwwroot/worker_venv"
+
+PKGS_36 = "lib/python3.6/site-packages"
+PKGS_37 = "lib/python3.7/site-packages"
--- azure_functions_worker/main.py
+++ azure_functions_worker/main.py
@@ -2,10 +2,14 @@
 
 
 import argparse
+import sys
+import os
+from pathlib import Path
 
 from . import aio_compat
 from . import dispatcher
 from . import logging
+from . import constants
 from .logging import error_logger, logger
 
 
@@ -26,8 +30,28 @@ def parse_args():
     return parser.parse_args()
 
 
+def setup_user_pkg_paths():
+    minor_version = sys.version_info[1]
+
+    home = Path.home()
+    pkgs_path = os.path.join(home, constants.PKGS_PATH)
+    venv_pkgs_path = os.path.join(home, constants.VENV_PKGS_PATH)
+
+    if minor_version == 6:
+        sys.path.insert(1, os.path.join(venv_pkgs_path, constants.PKGS_36))
+        sys.path.insert(1, os.path.join(pkgs_path, constants.PKGS_36))
+    elif minor_version == 7:
+        sys.path.insert(1, os.path.join(venv_pkgs_path, constants.PKGS_37))
+        sys.path.insert(1, os.path.join(pkgs_path, constants.PKGS_37))
+    else:
+        raise RuntimeError(f'Unsupported Python version: 3.{minor_version}')
+
+    logger.info(f'Added user package paths for Python 3.{minor_version}')
+
+
 def main():
     args = parse_args()
+    setup_user_pkg_paths()
     logging.setup(log_level=args.log_level, log_destination=args.log_to)
 
     logger.info('Starting Azure Functions Python Worker.')

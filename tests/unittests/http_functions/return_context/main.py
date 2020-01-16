d85551ff8696201c405339dd2b15f28e5e8f1a77
yojagad <yojagad@microsoft.com>
2020-01-15 19:09:48 -0800

    Testing

--- tests/unittests/http_functions/return_context/main.py
+++ tests/unittests/http_functions/return_context/main.py
@@ -4,6 +4,10 @@ import azure.functions
 
 
 def main(req: azure.functions.HttpRequest, context: azure.functions.Context):
+    attributePresent = 'false'
+    if Attributes in context.trace_context:
+        attributePresent = 'true'
+
     return json.dumps({
         'method': req.method,
         'ctx_func_name': context.function_name,
@@ -11,5 +15,5 @@ def main(req: azure.functions.HttpRequest, context: azure.functions.Context):
         'ctx_invocation_id': context.invocation_id,
         'ctx_trace_context_Traceparent': context.trace_context.Traceparent,
         'ctx_trace_context_Tracestate' : context.trace_context.Tracestate,
-        'ctx_trace_context_Attributes' : context.trace_context.Attributes,
+        'ctx_trace_context_Attributes_Present' : attributePresent,
     })

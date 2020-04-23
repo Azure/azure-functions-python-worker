9e70e3a019a01e9f47cd70e14a4688e8389c66fe
Hanzhang Zeng <48038149+Hazhzeng@users.noreply.github.com>
2020-04-23 11:58:08 -0700

    Move implicit output bindings serialization logic to Python library (#643)
    * Use json for serialization
    
    * Move implicit output binding serializer to python-library
    
    * Add unittests for primitive return types in activity triggers
    
    * Remove unnecessary comments in test cases
    
    * Fetch latest Azure Functions Python library from test.pypi.org

--- .ci/linux_devops_build.sh
+++ .ci/linux_devops_build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 set -e -x
-      
+
+# Install the latest Azure Functions Python Worker from test.pypi.org
 python -m pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple -U -e .[dev]
+
+# Install the latest Azure Functions Python Library from test.pypi.org
+python -m pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple -U azure-functions --pre
+
+# Download Azure Functions Host
 python setup.py webhost

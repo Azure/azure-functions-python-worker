25c26d9f64f15d87ea69ecabd8ed29c3575e4892
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2020-06-26 14:20:49 -0700

    Remove duplicated headers

--- azure_functions_worker/bindings/__init__.py
+++ azure_functions_worker/bindings/__init__.py
@@ -1,7 +1,5 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
 from .tracecontext import TraceContext
 from .context import Context
 from .meta import check_input_type_annotation

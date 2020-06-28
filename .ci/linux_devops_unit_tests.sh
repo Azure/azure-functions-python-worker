a02282512ee8fc98f2e2c7be56ea9b01d7f64e0d
Varad Meru [gmail] <varad.meru@gmail.com>
2020-06-28 15:08:35 -0700

    Add --cov-branch option

--- .ci/linux_devops_unit_tests.sh
+++ .ci/linux_devops_unit_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -e -x
-pytest  --instafail --cov=./azure_functions_worker --cov-report xml tests/unittests
+pytest  --instafail --cov=./azure_functions_worker --cov-report xml --cov-branch tests/unittests

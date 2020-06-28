9635e809abb3f733a6e73846382bd1caf0141283
Varad Meru [gmail] <varad.meru@gmail.com>
2020-06-28 15:17:52 -0700

    Removing parallelization in E2E case

--- .ci/linux_devops_e2e_tests.sh
+++ .ci/linux_devops_e2e_tests.sh
@@ -6,4 +6,4 @@ export AzureWebJobsCosmosDBConnectionString=$LINUXCOSMOSDBCONNECTIONSTRING
 export AzureWebJobsEventHubConnectionString=$LINUXEVENTHUBCONNECTIONSTRING
 export AzureWebJobsServiceBusConnectionString=$LINUXSERVICEBUSCONNECTIONSTRING
 
-pytest  --instafail -n 2 --cov=./azure_functions_worker --cov-report xml --cov-branch --cov-append tests/endtoend
+pytest  --instafail --cov=./azure_functions_worker --cov-report xml --cov-branch --cov-append tests/endtoend

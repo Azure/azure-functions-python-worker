d49405c13bfe4738552be70467511a06e227c542
Leighton <lechen@microsoft.com>
2020-10-14 18:54:08 -0400

    Update test_extensions.py

--- tests/unittests/test_extensions.py
+++ tests/unittests/test_extensions.py
@@ -16,13 +16,13 @@ class TestExtensions(TestCase):
         mock_cb2 = mock.Mock()
         extensions.register_before_invocation_request(mock_cb)
         self.assertEqual(
-            extensions._EXTENSIONS_CONTEXT \
+            extensions._EXTENSIONS_CONTEXT
             ["BEFORE_INVOCATION_REQUEST_CALLBACKS"][0],
             mock_cb,
         )
         extensions.register_before_invocation_request(mock_cb2)
         self.assertEqual(
-            extensions._EXTENSIONS_CONTEXT \
+            extensions._EXTENSIONS_CONTEXT
             ["BEFORE_INVOCATION_REQUEST_CALLBACKS"][1],
             mock_cb2,
         )
@@ -32,13 +32,13 @@ class TestExtensions(TestCase):
         mock_cb2 = mock.Mock()
         extensions.register_after_invocation_request(mock_cb)
         self.assertEqual(
-            extensions._EXTENSIONS_CONTEXT \
+            extensions._EXTENSIONS_CONTEXT
             ["AFTER_INVOCATION_REQUEST_CALLBACKS"][0],
             mock_cb,
         )
         extensions.register_after_invocation_request(mock_cb2)
         self.assertEqual(
-            extensions._EXTENSIONS_CONTEXT \
+            extensions._EXTENSIONS_CONTEXT
             ["AFTER_INVOCATION_REQUEST_CALLBACKS"][1],
             mock_cb2,
         )
@@ -47,13 +47,13 @@ class TestExtensions(TestCase):
         mock_cb = mock.Mock()
         extensions.register_before_invocation_request(mock_cb)
         self.assertEqual(
-            extensions._EXTENSIONS_CONTEXT \
+            extensions._EXTENSIONS_CONTEXT
             ["BEFORE_INVOCATION_REQUEST_CALLBACKS"][0],
             mock_cb,
         )
         extensions.clear_before_invocation_request_callbacks()
         self.assertIsNone(
-            extensions._EXTENSIONS_CONTEXT. \
+            extensions._EXTENSIONS_CONTEXT.
             get("BEFORE_INVOCATION_REQUEST_CALLBACKS"),
         )
 
@@ -61,13 +61,13 @@ class TestExtensions(TestCase):
         mock_cb = mock.Mock()
         extensions.register_after_invocation_request(mock_cb)
         self.assertEqual(
-            extensions._EXTENSIONS_CONTEXT \
+            extensions._EXTENSIONS_CONTEXT
             ["AFTER_INVOCATION_REQUEST_CALLBACKS"][0],
             mock_cb,
         )
         extensions.clear_after_invocation_request_callbacks()
         self.assertIsNone(
-            extensions._EXTENSIONS_CONTEXT. \
+            extensions._EXTENSIONS_CONTEXT.
             get("AFTER_INVOCATION_REQUEST_CALLBACKS"),
         )
 
@@ -75,7 +75,7 @@ class TestExtensions(TestCase):
         mock_cb = mock.Mock()
         extensions.register_before_invocation_request(mock_cb)
         self.assertEqual(
-            extensions._EXTENSIONS_CONTEXT \
+            extensions._EXTENSIONS_CONTEXT
             ["BEFORE_INVOCATION_REQUEST_CALLBACKS"][0],
             mock_cb,
         )
@@ -88,7 +88,7 @@ class TestExtensions(TestCase):
         mock_cb = mock.Mock()
         extensions.register_after_invocation_request(mock_cb)
         self.assertEqual(
-            extensions._EXTENSIONS_CONTEXT \
+            extensions._EXTENSIONS_CONTEXT
             ["AFTER_INVOCATION_REQUEST_CALLBACKS"][0],
             mock_cb,
         )

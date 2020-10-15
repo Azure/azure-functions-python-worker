94bef0200b6ec9295dbaa8a2545f239303d734fb
Gohar Irfan Chaudhry <gochaudh@microsoft.com>
2020-10-15 13:22:06 +0500

    Writing output from worker to Shared Memory

--- azure_functions_worker/bindings/shared_memory_manager.py
+++ azure_functions_worker/bindings/shared_memory_manager.py
@@ -1,14 +1,40 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from .. import protos
+import uuid
 from ..logging import logger
 
 class SharedMemoryManager:
+    """
+    Performs all operations related to reading/writing data from/to Shared
+    Memory.
+    """
     def __init__(self):
         pass
 
-    def get(self, td: protos.SharedMemoryData):
-        logger.info('Reading from Shared Memory: %s', td)
-        print('Reading from Shared Memory: %s' % td)
-        return 'foo'.encode('utf-8'), td.type
+    @staticmethod
+    def is_enabled():
+        """
+        Whether supported types should be transferred between Functions host
+        and the worker using Shared Memory.
+        """
+        return True
+
+    def get(self, mmap_name: str, offset: int, count: int) -> (bytes):
+        """
+        Reads data from the given Memory Mapped File with the provided name,
+        starting at the provided offset and reading a total of count bytes.
+        Returns a tuple containing the binary data read from Shared Memory
+        if successful, None otherwise.
+        """
+        logger.info('Reading from Shared Memory: %s', mmap_name)
+        return 'foo'.encode('utf-8')
+
+    def put(self, data: bytes) -> (str):
+        """
+        Writes the given data into Shared Memory.
+        Returns the name of the Memory Mapped File into which the data was
+        written if succesful, None otherwise.
+        """
+        mmap_name = str(uuid.uuid4())
+        return mmap_name

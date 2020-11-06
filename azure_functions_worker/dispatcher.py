9c87d38ab769bb943db412cafbad045e9aabc6f3
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2020-11-05 16:52:03 -0800

    Make sync call tp allocation explicit

--- azure_functions_worker/dispatcher.py
+++ azure_functions_worker/dispatcher.py
@@ -77,11 +77,10 @@ class Dispatcher(metaclass=DispatcherMeta):
 
         # We allow the customer to change synchronous thread pool count by
         # PYTHON_THREADPOOL_THREAD_COUNT app setting. The default value is 1.
-        self._sync_tp_max_workers: int = PYTHON_THREADPOOL_THREAD_COUNT_DEFAULT
-        self._sync_call_tp: Optional[concurrent.futures.Executor] = None
-
-        sync_tp_max_worker: int = self._get_sync_tp_max_workers()
-        self._create_or_update_sync_call_tp(sync_tp_max_worker)
+        self._sync_tp_max_workers: int = self._get_sync_tp_max_workers()
+        self._sync_call_tp: concurrent.futures.Executor = (
+            self._create_sync_call_tp(self._sync_tp_max_workers)
+        )
 
         self._grpc_connect_timeout: float = grpc_connect_timeout
         # This is set to -1 by default to remove the limitation on msg size
@@ -432,8 +431,10 @@ class Dispatcher(metaclass=DispatcherMeta):
                 os.environ[var] = env_vars[var]
 
             # Apply PYTHON_THREADPOOL_THREAD_COUNT
-            sync_tp_max_worker: int = self._get_sync_tp_max_workers()
-            self._create_or_update_sync_call_tp(sync_tp_max_worker)
+            self._sync_tp_max_workers = self._get_sync_tp_max_workers()
+            self._sync_call_tp = (
+                self._create_sync_call_tp(self._sync_tp_max_workers)
+            )
 
             # Reload package namespaces for customer's libraries
             packages_to_reload = ['azure', 'google']
@@ -518,15 +519,12 @@ class Dispatcher(metaclass=DispatcherMeta):
             default_value=f'{PYTHON_THREADPOOL_THREAD_COUNT_DEFAULT}',
             validator=tp_max_workers_validator))
 
-    def _create_or_update_sync_call_tp(self, max_worker: int):
+    def _create_sync_call_tp(
+            self, max_worker: int) -> concurrent.futures.Executor:
         """Update the self._sync_call_tp executor with the proper max_worker.
-        Side effects:
-            - Update self._sync_tp_max_workers to max_worker
-            - Update self._sync_call_tp to have max_worker number of threads
         """
         self._stop_sync_call_tp()
-        self._sync_tp_max_workers = max_worker
-        self._sync_call_tp = concurrent.futures.ThreadPoolExecutor(
+        return concurrent.futures.ThreadPoolExecutor(
             max_workers=max_worker
         )
 

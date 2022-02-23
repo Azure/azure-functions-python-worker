185f38d37776579349c2fddc7614b4562fd9ed09
Varad <varad.meru@gmail.com>
2022-02-22 23:00:18 -0800

    Updating dependency versions and cleaning up setup.py

--- tests/unittests/resources/customer_func_path/func_specific_module/__init__.py
+++ tests/unittests/resources/customer_func_path/func_specific_module/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-__version__: str == 'function_app'
+__version__: str = 'function_app'
 
 import os
 # ./tests/unittests/resources/customer_func_path/func_specific_module

98053f1315954108846558ecdb402dba12312dd0
gavin-aguiar <80794152+gavin-aguiar@users.noreply.github.com>
2022-06-15 12:42:24 -0500

    Removed library imports from the worker (#1046)
    * Fix ModuleNotFoundError for older pinned functions versions
    
    * Organising imports
    
    * Fixed unit tests
    
    * Added unit test
    
    * Added unit tests
    
    * Remove library imports

--- azure_functions_worker/functions.py
+++ azure_functions_worker/functions.py
@@ -5,8 +5,6 @@ import operator
 import pathlib
 import typing
 
-from azure.functions import DataType, Function
-
 from . import bindings as bindings_utils
 from . import protos
 from ._thirdparty import typing_inspect
@@ -216,8 +214,7 @@ class Registry:
                         param_bind_type, param_py_type)
 
                 if not checks_out:
-                    if binding.data_type is not DataType(
-                            protos.BindingInfo.undefined):
+                    if binding.data_type is not protos.BindingInfo.undefined:
                         raise FunctionLoadError(
                             func_name,
                             f'{param.name!r} binding type "{binding.type}" '
@@ -357,7 +354,7 @@ class Registry:
                                                       output_types, return_type)
 
     def add_indexed_function(self, function_id: str,
-                             function: Function):
+                             function):
         func = function.get_user_function()
         func_name = function.get_function_name()
         return_binding_name: typing.Optional[str] = None

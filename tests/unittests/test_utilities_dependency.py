5244055b0623fa330c550e8e2cda32f1e177a639
Gavin Aguiar <gaaguiar@microsoft.com>
2022-06-16 10:40:36 -0500

    Added unit tests

--- tests/unittests/test_utilities_dependency.py
+++ tests/unittests/test_utilities_dependency.py
@@ -608,6 +608,19 @@ class TestDependencyManager(unittest.TestCase):
             os.path.join(self._customer_func_path, 'func_specific_module')
         )
 
+    def test_reload_customer_libraries(self):
+        os.environ['PYTHON_ISOLATE_WORKER_DEPENDENCIES'] = 'true'
+        # Setup paths
+        DependencyManager.worker_deps_path = self._worker_deps_path
+        DependencyManager.cx_deps_path = self._customer_deps_path
+        DependencyManager.cx_working_dir = self._customer_func_path
+
+        DependencyManager.reload_customer_libraries(self._customer_deps_path)
+        import common_module  # NoQA
+        self.assertEqual(
+            common_module.package_location,
+            os.path.join(self._customer_deps_path, 'common_module'))
+
     def test_remove_module_cache(self):
         # First import the common_module and create a sys.modules cache
         sys.path.append(self._customer_deps_path)

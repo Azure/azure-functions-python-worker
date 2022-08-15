00a907bcb6de686412be33da9371ed1f0ad10b1a
gavin-aguiar <80794152+gavin-aguiar@users.noreply.github.com>
2022-08-15 10:40:52 -0500

    Updated stein tests in include new binding names (#1078)
    * Update stein tests for new binding names
    
    * Fixed flake8 errors

--- tests/endtoend/table_functions/table_functions_stein/function_app.py
+++ tests/endtoend/table_functions/table_functions_stein/function_app.py
@@ -9,11 +9,11 @@ app = func.FunctionApp()
 
 @app.function_name(name="table_in_binding")
 @app.route(route="table_in_binding/{id}")
-@app.read_table(arg_name="testEntity",
-                connection="AzureWebJobsStorage",
-                table_name="BindingTestTable",
-                row_key='{id}',
-                partition_key="test")
+@app.table_input(arg_name="testEntity",
+                 connection="AzureWebJobsStorage",
+                 table_name="BindingTestTable",
+                 row_key='{id}',
+                 partition_key="test")
 def table_in_binding(req: func.HttpRequest, testEntity):
     headers_dict = json.loads(testEntity)
     return func.HttpResponse(status_code=200, headers=headers_dict)
@@ -21,9 +21,9 @@ def table_in_binding(req: func.HttpRequest, testEntity):
 
 @app.function_name(name="table_out_binding")
 @app.route(route="table_out_binding", binding_arg_name="resp")
-@app.write_table(arg_name="$return",
-                 connection="AzureWebJobsStorage",
-                 table_name="BindingTestTable")
+@app.table_output(arg_name="$return",
+                  connection="AzureWebJobsStorage",
+                  table_name="BindingTestTable")
 def table_out_binding(req: func.HttpRequest, resp: func.Out[func.HttpResponse]):
     row_key_uuid = str(uuid.uuid4())
     table_dict = {'PartitionKey': 'test', 'RowKey': row_key_uuid}

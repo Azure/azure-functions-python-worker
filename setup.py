a2405042c5eaf0b66a1a4142981db7825cb06803
gavin-aguiar <80794152+gavin-aguiar@users.noreply.github.com>
2022-08-18 16:25:43 -0500

    Updated quotes

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ INSTALL_REQUIRES = [
     "grpcio~=1.43.0",
     "grpcio-tools~=1.43.0",
     "protobuf~=3.19.3",
-    'azure-functions==1.11.3',
+    "azure-functions==1.11.3",
     "python-dateutil~=2.8.2"
 ]
 

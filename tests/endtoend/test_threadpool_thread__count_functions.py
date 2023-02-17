9f593cea94faffec9146bbb08294dca8b36189da
pdthummar <101662222+pdthummar@users.noreply.github.com>
2023-02-17 12:25:22 -0600

    removed tests for worker and threadpool count 1.

--- tests/endtoend/test_threadpool_thread__count_functions.py
+++ tests/endtoend/test_threadpool_thread__count_functions.py
@@ -7,63 +7,6 @@ from datetime import datetime
 from tests.utils import testutils
 
 
-class TestPythonThreadpoolThreadCount1(testutils.WebHostTestCase):
-    """ Test the Http Trigger with setting up the python threadpool thread
-    count to 1.this test will check if both requests should not be processed
-    at the same time. this file is more focus on testing the E2E flow
-    scenarios.
-    """
-
-    @classmethod
-    def setUpClass(cls):
-        os_environ = os.environ.copy()
-        os_environ['PYTHON_THREADPOOL_THREAD_COUNT'] = '1'
-        cls._patch_environ = patch.dict('os.environ', os_environ)
-        cls._patch_environ.start()
-        super().setUpClass()
-
-    def tearDown(self):
-        super().tearDown()
-        self._patch_environ.stop()
-
-    @classmethod
-    def get_script_dir(cls):
-        return testutils.E2E_TESTS_FOLDER / 'http_functions'
-
-    @testutils.retryable_test(3, 5)
-    def test_http_func_with_thread_count_1(self):
-        res = [None, None]
-
-        def http_req(response_num):
-            r = self.webhost.request('GET', 'http_func')
-            self.assertTrue(r.ok)
-            res[response_num] = datetime.strptime(
-                r.content.decode("utf-8"), "%H:%M:%S")
-
-        # creating 2 different threads to send parallel HTTP request
-        trd1 = Thread(target=http_req, args=(0,))
-        trd2 = Thread(target=http_req, args=(1,))
-        trd1.start()
-        trd2.start()
-        trd1.join()
-        trd2.join()
-        """function execution time difference between both HTTP request
-        should be greater than of equal to 1 since both the request should
-        not be processed at the same time because
-        PYTHON_THREADPOOL_THREAD_COUNT is 1.
-        """
-        time_diff_in_seconds = abs((res[0] - res[1]).total_seconds())
-        self.assertTrue(time_diff_in_seconds >= 1)
-
-
-class TestPythonThreadpoolThreadCount1Stein(TestPythonThreadpoolThreadCount1):
-
-    @classmethod
-    def get_script_dir(cls):
-        return testutils.E2E_TESTS_FOLDER / 'http_functions' / \
-                                            'http_functions_stein'
-
-
 class TestPythonThreadpoolThreadCount2(testutils.WebHostTestCase):
     """ Test the Http Trigger with setting up the python threadpool thread
     count to 2. this test will check if both requests should be processed
--- tests/endtoend/test_worker_proccess_count_functions.py
+++ tests/endtoend/test_worker_proccess_count_functions.py
@@ -7,64 +7,6 @@ from datetime import datetime
 from tests.utils import testutils
 
 
-class TestWorkerProcessCount1(testutils.WebHostTestCase):
-    """Test the Http Trigger with setting up the python worker process count
-    to 1. this test will check if both requests should not be processed at
-    the same time. this file is more focused on testing the E2E flow scenario
-    for FUNCTIONS_WORKER_PROCESS_COUNT feature.
-    """
-
-    @classmethod
-    def setUpClass(cls):
-        os_environ = os.environ.copy()
-        os_environ['PYTHON_THREADPOOL_THREAD_COUNT'] = '1'
-        os_environ['FUNCTIONS_WORKER_PROCESS_COUNT'] = '1'
-        cls._patch_environ = patch.dict('os.environ', os_environ)
-        cls._patch_environ.start()
-        super().setUpClass()
-
-    def tearDown(self):
-        super().tearDown()
-        self._patch_environ.stop()
-
-    @classmethod
-    def get_script_dir(cls):
-        return testutils.E2E_TESTS_FOLDER / 'http_functions'
-
-    @testutils.retryable_test(3, 5)
-    def test_http_func_with_worker_process_count_1(self):
-        res = [None, None]
-
-        def http_req(response_num):
-            r = self.webhost.request('GET', 'http_func')
-            self.assertTrue(r.ok)
-            res[response_num] = datetime.strptime(
-                r.content.decode("utf-8"), "%H:%M:%S")
-
-        # creating 2 different threads to send HTTP request
-        trd1 = Thread(target=http_req, args=(0,))
-        trd2 = Thread(target=http_req, args=(1,))
-        trd1.start()
-        trd2.start()
-        trd1.join()
-        trd2.join()
-        """function execution time difference between both HTTP request
-        should be greater than of equal to 1 since both the request should
-        not be processed at the same time because
-        FUNCTIONS_WORKER_PROCESS_COUNT is 1.
-        """
-        time_diff_in_seconds = abs((res[0] - res[1]).total_seconds())
-        self.assertTrue(time_diff_in_seconds >= 1)
-
-
-class TestWorkerProcessCount1Stein(TestWorkerProcessCount1):
-
-    @classmethod
-    def get_script_dir(cls):
-        return testutils.E2E_TESTS_FOLDER / 'http_functions' /\
-                                            'http_functions_stein'
-
-
 class TestWorkerProcessCount2(testutils.WebHostTestCase):
     """Test the Http Trigger with setting up the python worker process count
     to 2. this test will check if both requests should be processed at the

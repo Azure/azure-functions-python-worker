4bb5f5feb4ee6adc9ef72a4794837df4ce0ac4ee
Evan Roman <evanroman@microsoft.com>
2023-04-08 13:22:22 -0500

    Retry

--- .ci/linux_devops_e2e_tests.sh
+++ .ci/linux_devops_e2e_tests.sh
@@ -9,5 +9,4 @@ export AzureWebJobsSqlConnectionString=$LINUXSQLCONNECTIONSTRING
 export AzureWebJobsEventGridTopicUri=$LINUXEVENTGRIDTOPICURI
 export AzureWebJobsEventGridConnectionKey=$LINUXEVENTGRIDTOPICCONNECTIONKEY
 
-coverage run -p --branch -m pytest tests/endtoend
-#pytest  --instafail --cov=./azure_functions_worker --cov-report xml --cov-branch --cov-append tests/endtoend
+pytest  --instafail --cov=./azure_functions_worker --cov-report xml --cov-branch --cov-append tests/endtoend

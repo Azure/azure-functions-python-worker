d56092dbd295ee17982836a2e48b24c52534a623
Gavin Aguiar <gavin@GavinPC>
2023-05-01 20:27:53 -0500

    Updated cosmosdb functions to use v4 extension

--- tests/endtoend/cosmosdb_functions/cosmosdb_functions_stein/function_app.py
+++ tests/endtoend/cosmosdb_functions/cosmosdb_functions_stein/function_app.py
@@ -8,19 +8,19 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @app.route()
 @app.cosmos_db_input(
     arg_name="docs", database_name="test",
-    collection_name="items",
+    container_name="items",
     id="cosmosdb-input-test",
-    connection_string_setting="AzureWebJobsCosmosDBConnectionString")
+    connection="AzureWebJobsCosmosDBConnectionString")
 def cosmosdb_input(req: func.HttpRequest, docs: func.DocumentList) -> str:
     return func.HttpResponse(docs[0].to_json(), mimetype='application/json')
 
 
 @app.cosmos_db_trigger(
     arg_name="docs", database_name="test",
-    collection_name="items",
-    lease_collection_name="leases",
-    connection_string_setting="AzureWebJobsCosmosDBConnectionString",
-    create_lease_collection_if_not_exists=True)
+    container_name="items",
+    lease_container_name="leases",
+    connection="AzureWebJobsCosmosDBConnectionString",
+    create_lease_container_if_not_exists=True)
 @app.blob_output(arg_name="$return", connection="AzureWebJobsStorage",
                  path="python-worker-tests/test-cosmosdb-triggered.txt")
 def cosmosdb_trigger(docs: func.DocumentList) -> str:
@@ -38,10 +38,9 @@ def get_cosmosdb_triggered(req: func.HttpRequest,
 @app.route()
 @app.cosmos_db_output(
     arg_name="doc", database_name="test",
-    collection_name="items",
+    container_name="items",
     create_if_not_exists=True,
-    connection_string_setting="AzureWebJobsCosmosDBConnectionString")
+    connection="AzureWebJobsCosmosDBConnectionString")
 def put_document(req: func.HttpRequest, doc: func.Out[func.Document]):
     doc.set(func.Document.from_json(req.get_body()))
-
     return 'OK'

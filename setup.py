23eab3096322f0c88554ed6d6a458e1241686be3
pthummar <pthummar@microsoft.com>
2023-06-15 16:36:34 -0500

    test1

--- setup.py
+++ setup.py
@@ -138,7 +138,7 @@ EXTRA_REQUIRES = {
         "pytest-randomly",
         "pytest-instafail",
         "pytest-rerunfailures",
-        "pytest-ordering"
+        "pytest-ordering",
         "ptvsd",
         "python-dotenv",
         "plotly",

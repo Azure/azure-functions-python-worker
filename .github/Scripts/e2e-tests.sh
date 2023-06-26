507e6352c45ff8bb676a1eac5ce85a89c6238b87
Gavin Aguiar <gavin@GavinPC>
2023-06-26 13:25:30 -0500

    Revert "Update e2e tests GH workflow to fix flaky tests. (#1276)"
    This reverts commit cd16cf0a05397732b2c564fa7625b39a2a4d3fa7.

--- .github/Scripts/e2e-tests.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-python -m pytest -n auto --dist loadfile --reruns 4 -vv --instafail --cov=./azure_functions_worker --cov-report xml --cov-branch --cov-append tests/endtoend/test_worker_process_count_functions.py tests/endtoend/test_threadpool_thread_count_functions.py
-python -m pytest -n auto --dist loadfile --reruns 4 -vv --instafail --cov=./azure_functions_worker --cov-report xml --cov-branch --cov-append --ignore=tests/endtoend/test_worker_process_count_functions.py --ignore=tests/endtoend/test_threadpool_thread_count_functions.py tests/endtoend

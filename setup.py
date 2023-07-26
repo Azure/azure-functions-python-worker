a8a4d42bd502bb3a56082aa8a62c897e244d2dd6
gavin-aguiar <80794152+gavin-aguiar@users.noreply.github.com>
2023-07-26 09:56:12 -0500

    Update Python SDK Version to 1.15.1b2 (#1293)
    * Update Python SDK Version to 1.15.1b2
    
    * Update setup.py
    
    ---------
    
    Co-authored-by: AzureFunctionsPython <funcdisc@microsoft.com>

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ PACKAGES = [
 ]
 
 INSTALL_REQUIRES = [
-    "azure-functions==1.15.1b1",
+    "azure-functions==1.15.1b2",
     "python-dateutil~=2.8.2",
     "protobuf~=4.22.0",
     "grpcio-tools~=1.54.2",

370db9e0a0ba3e80a19f2348e394d7ad9086098d
peterstone2017 <12449837+YunchuWang@users.noreply.github.com>
2023-11-13 01:30:34 -0500

    worker refactor initial

--- azure_functions_worker/utils/wrappers.py
+++ azure_functions_worker/utils/wrappers.py
@@ -1,12 +1,42 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-
+import functools
 from typing import Callable, Any
 
 from .common import is_envvar_true, is_envvar_false
 from .tracing import extend_exception_message
 from ..logging import logger
 
+
+# def handle_invocation_before_and_after(f):
+#     @functools.wraps(f)
+#     async def wrapper(self, request):
+#         # Pre-processing code here
+#         print("Pre-processing before the main function call.")
+#         invocation_id = request.invocation_request.invocation_id
+#         function_id = request.invocation_request.function_id
+#         fi: functions.FunctionInfo = self._functions.get_function(
+#             function_id)
+#         is_http_trigger_func = await self.is_http_trigger_func(fi)
+#         if not is_http_trigger_func:
+#             return await f(self, request)
+#
+#         http_request = await http_coordinator.wait_and_get_http_invoc_request(
+#             invocation_id)
+#         args["req"] = http_request
+#
+#         try:
+#             # Call the async function
+#             result = await f(self, request)
+#             # Post-processing code here
+#             print("Post-processing after the main function call.")
+#             return result
+#         except Exception as e:
+#             # You can add exception handling if needed
+#             print("An error occurred: ", e)
+#             raise  # Re-raise the exception
+#
+#     return wrapper
 
 def enable_feature_by(flag: str,
                       default: Any = None,
@@ -18,7 +48,9 @@ def enable_feature_by(flag: str,
             if flag_default and not is_envvar_false(flag):
                 return func(*args, **kwargs)
             return default
+
         return call
+
     return decorate
 
 
@@ -32,7 +64,9 @@ def disable_feature_by(flag: str,
             if flag_default and not is_envvar_false(flag):
                 return default
             return func(*args, **kwargs)
+
         return call
+
     return decorate
 
 
@@ -46,5 +80,7 @@ def attach_message_to_exception(expt_type: Exception, message: str,
                 if debug_logs is not None:
                     logger.error(debug_logs)
                 raise extend_exception_message(e, message)
+
         return call
+
     return decorate

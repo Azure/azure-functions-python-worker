d632231ec78283fb4ebbfc9424d7b936f67b6454
hallvictoria <victoriahall@microsoft.com>
2023-12-20 10:46:20 -0600

    timeout in durable tests

--- tests/endtoend/test_durable_functions.py
+++ tests/endtoend/test_durable_functions.py
@@ -47,7 +47,7 @@ class TestDurableFunctions(testutils.WebHostTestCase):
 
     def test_durable(self):
         r = self.webhost.request('GET',
-                                 'orchestrators/DurableFunctionsOrchestrator')
+                                 'orchestrators/DurableFunctionsOrchestrator', timeout=5)
         time.sleep(4)  # wait for the activity to complete
         self.assertEqual(r.status_code, 202)
         content = json.loads(r.content)

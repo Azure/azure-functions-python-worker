e1e9c1bd560257e148be5530f44d29883b434a96
hallvictoria <victoriahall@microsoft.com>
2024-01-30 15:47:05 -0600

    fixing failing eventhub batch test

--- tests/endtoend/eventhub_batch_functions/get_eventhub_batch_triggered/__init__.py
+++ tests/endtoend/eventhub_batch_functions/get_eventhub_batch_triggered/__init__.py
@@ -1,8 +1,11 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+import json
+
 import azure.functions as func
 
 
 # Retrieve the event data from storage blob and return it as Http response
 def main(req: func.HttpRequest, testEntities):
-    return func.HttpResponse(status_code=200, body=testEntities)
+    headers_dict = json.loads(testEntities)
+    return func.HttpResponse(status_code=200, body=headers_dict)

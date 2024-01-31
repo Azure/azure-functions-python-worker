b864c3187f3b097ac8e0dd769cc1ed134e1097b6
Victoria Hall <victoriahall@microsoft.com>
2024-01-31 15:58:48 -0600

    eventhub output binding

--- tests/endtoend/eventhub_batch_functions/eventhub_output_batch/__init__.py
+++ tests/endtoend/eventhub_batch_functions/eventhub_output_batch/__init__.py
@@ -4,6 +4,7 @@ import azure.functions as func
 
 
 # An HttpTrigger to generating EventHub event from EventHub Output Binding
-def main(req: func.HttpRequest, out: func.Out[str]) -> str:
+def main(req: func.HttpRequest, out: func.Out[str]) -> func.HttpResponse:
     events = req.get_body().decode('utf-8')
-    return events
+    out.set(events)
+    return func.HttpResponse(f"Messages sent")

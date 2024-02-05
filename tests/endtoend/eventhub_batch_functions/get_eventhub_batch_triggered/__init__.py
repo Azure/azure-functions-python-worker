d3aa3402135ba4f8605af246dd7d49f1c7502eb7
hallvictoria <victoriahall@microsoft.com>
2024-02-05 12:10:00 -0600

    changed param name

--- tests/endtoend/eventhub_batch_functions/get_eventhub_batch_triggered/__init__.py
+++ tests/endtoend/eventhub_batch_functions/get_eventhub_batch_triggered/__init__.py
@@ -4,5 +4,5 @@ import azure.functions as func
 
 
 # Retrieve the event data from storage blob and return it as Http response
-def main(req: func.HttpRequest, testEntities):
-    return func.HttpResponse(status_code=200, body=testEntities)
+def main(req: func.HttpRequest, testEntity):
+    return func.HttpResponse(status_code=200, body=testEntity)

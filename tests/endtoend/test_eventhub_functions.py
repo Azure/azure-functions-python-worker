58718ecc26339515a2e469639cb1655ee1287c5f
hallvictoria <victoriahall@microsoft.com>
2024-02-08 12:01:12 -0600

    flake

--- tests/endtoend/test_eventhub_functions.py
+++ tests/endtoend/test_eventhub_functions.py
@@ -1,7 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 import json
-import dateutil.parser as parser
 import time
 
 from tests.utils import testutils

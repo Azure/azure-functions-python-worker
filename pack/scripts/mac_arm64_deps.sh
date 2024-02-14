30ce1af8c8b6eb218426400630ffc883d8287b95
gavin-aguiar <gaaguiar@microsoft.com>
2024-02-14 13:58:01 -0600

    Updated arm pack script

--- pack/scripts/mac_arm64_deps.sh
+++ pack/scripts/mac_arm64_deps.sh
@@ -2,6 +2,13 @@
 
 python -m venv .env
 source .env/bin/activate
+
+PYTHON_VERSION=$(python --version 2>&1)
+
+if [[ $PYTHON_VERSION == *"3.12"* ]]; then
+  python -m ensurepip --upgrade
+fi
+
 python -m pip install --upgrade pip==23.0
 
 python -m pip install .

0bffac4d8bbe1b915c85dd256b6d173bfe8eb3ae
gavin-aguiar <gaaguiar@microsoft.com>
2024-02-14 14:18:58 -0600

    Printing the python version

--- pack/scripts/mac_arm64_deps.sh
+++ pack/scripts/mac_arm64_deps.sh
@@ -4,12 +4,14 @@ python -m venv .env
 source .env/bin/activate
 
 PYTHON_VERSION=$(python --version 2>&1)
+echo "Python Version: $PYTHON_VERSION"
 
 if [[ $PYTHON_VERSION == *"3.12"* ]]; then
   python -m ensurepip --upgrade
+else
+  python -m pip install --upgrade pip==23.0
 fi
 
-python -m pip install --upgrade pip==23.0
 
 python -m pip install .
 

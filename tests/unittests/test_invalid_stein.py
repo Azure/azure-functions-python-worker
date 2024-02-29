71c0169b569faec12c5e3716837c61eaaa2f8d24
hallvictoria <victoriahall@microsoft.com>
2024-02-29 10:57:23 -0600

    fixed invalid stein tests

--- tests/unittests/test_invalid_stein.py
+++ tests/unittests/test_invalid_stein.py
@@ -1,14 +1,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-import collections as col
-import os
-from unittest.mock import patch
-
 from azure_functions_worker import protos
 from tests.utils import testutils
 
-SysVersionInfo = col.namedtuple("VersionInfo", ["major", "minor", "micro",
-                                                "releaselevel", "serial"])
 STEIN_INVALID_APP_FUNCTIONS_DIR = testutils.UNIT_TESTS_FOLDER / \
     'broken_functions' / \
     'invalid_app_stein'
@@ -18,52 +12,40 @@ STEIN_INVALID_FUNCTIONS_DIR = testutils.UNIT_TESTS_FOLDER / \
 
 
 class TestInvalidAppStein(testutils.AsyncTestCase):
-
-    def setUp(self):
-        self._ctrl = testutils.start_mockhost(
-            script_root=STEIN_INVALID_APP_FUNCTIONS_DIR)
-        self._pre_env = dict(os.environ)
-        self.mock_version_info = patch(
-            'azure_functions_worker.dispatcher.sys.version_info',
-            SysVersionInfo(3, 9, 0, 'final', 0))
-        self.mock_version_info.start()
-
-    def tearDown(self):
-        os.environ.clear()
-        os.environ.update(self._pre_env)
-        self.mock_version_info.stop()
-
+    @testutils.retryable_test(4, 5)
     async def test_indexing_not_app(self):
-        """Test if the functions metadata response will be 0
-            when an invalid app is provided
-                """
-        async with self._ctrl as host:
+        """Test if the functions metadata response will be
+            an error when an invalid app is provided
+        """
+        async with testutils.start_mockhost(
+                script_root=STEIN_INVALID_APP_FUNCTIONS_DIR) as host:
+            await host.init_worker()
             r = await host.get_functions_metadata()
             self.assertIsInstance(r.response, protos.FunctionMetadataResponse)
-            self.assertIn("Error", r.response.result.exception.message)
+            self.assertEqual(r.response.result.status,
+                             protos.StatusResult.Failure)
+            self.assertIsNotNone(r.response.result.exception.message)
+            self.assertRegex(r.response.result.exception.message,
+                             r"ValueError: Could not find top level "
+                             r"function app instances in function_app.py.")
 
-
-class TestInvalidStein(testutils.AsyncTestCase):
-
-    def setUp(self):
-        self._ctrl = testutils.start_mockhost(
-            script_root=STEIN_INVALID_FUNCTIONS_DIR)
-        self._pre_env = dict(os.environ)
-        self.mock_version_info = patch(
-            'azure_functions_worker.dispatcher.sys.version_info',
-            SysVersionInfo(3, 9, 0, 'final', 0))
-        self.mock_version_info.start()
-
-    def tearDown(self):
-        os.environ.clear()
-        os.environ.update(self._pre_env)
-        self.mock_version_info.stop()
-
+    @testutils.retryable_test(4, 5)
     async def test_indexing_invalid_app(self):
-        """Test if the functions metadata response will be 0
-            when an invalid app is provided
-                """
-        async with self._ctrl as host:
+        """Test if the functions metadata response will be
+            an error when an invalid app is provided
+        """
+        async with testutils.start_mockhost(
+                script_root=STEIN_INVALID_FUNCTIONS_DIR) as host:
+            await host.init_worker()
             r = await host.get_functions_metadata()
             self.assertIsInstance(r.response, protos.FunctionMetadataResponse)
-            self.assertIn("Error", r.response.result.exception.message)
+            self.assertEqual(r.response.result.status,
+                             protos.StatusResult.Failure)
+            self.assertIsNotNone(r.response.result.exception.message)
+            self.assertRegex(r.response.result.exception.message,
+                             r"FunctionLoadError: cannot load the main "
+                             r"function: "
+                             r"the following parameters "
+                             r"are declared in function.json but not in "
+                             r"Python: "
+                             r".*'req'.*")

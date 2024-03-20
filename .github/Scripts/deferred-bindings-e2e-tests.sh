9d40991ee58c6c497320a7c9bc34ec7aca64e92e
Victoria Hall <victoria.hall@microsoft.com>
2024-03-20 11:41:37 -0500

    remove script

--- .github/Scripts/deferred-bindings-e2e-tests.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-python -m pytest -q -n auto --dist loadfile --reruns 4 --instafail --cov=./azure_functions_worker --cov-report xml --cov-branch --cov-append tests/extension_tests/deferred_bindings_tests

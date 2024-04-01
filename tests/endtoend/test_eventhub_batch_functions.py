eb40d46cb696d9be7f5fe14ebb5cd02b2f364e0d
hallvictoria <victoriahall@microsoft.com>
2024-04-01 15:33:53 -0500

    table & eh, unit, lint

--- tests/endtoend/test_eventhub_batch_functions.py
+++ tests/endtoend/test_eventhub_batch_functions.py
@@ -2,7 +2,6 @@
 # Licensed under the MIT License.
 import json
 import time
-import pathlib
 from datetime import datetime
 from unittest import skipIf
 
@@ -37,13 +36,8 @@ class TestEventHubFunctions(testutils.WebHostTestCase):
 
     def test_eventhub_multiple(self):
         NUM_EVENTS = 3
-        all_row_keys_seen = dict([(str(i), True) for i in range(NUM_EVENTS)])
+        all_row_keys_seen = dict([(i, True) for i in range(NUM_EVENTS)])
         partition_key = str(round(time.time()))
-
-        # Dynamically rewrite function.json to point to new partition key
-        # for recording EventHub state
-        old_partition_key = self._get_table_partition_key()
-        self._set_table_partition_key(partition_key)
 
         # wait for host to restart after change
         time.sleep(5)
@@ -57,29 +51,26 @@ class TestEventHubFunctions(testutils.WebHostTestCase):
                                  data=json.dumps(docs))
         self.assertEqual(r.status_code, 200)
 
-        row_keys = [str(i) for i in range(NUM_EVENTS)]
+        row_keys = [i for i in range(NUM_EVENTS)]
         seen = [False] * NUM_EVENTS
         row_keys_seen = dict(zip(row_keys, seen))
 
         # Allow trigger to fire.
         time.sleep(5)
 
-        try:
-            r = self.webhost.request('GET', 'get_eventhub_batch_triggered')
+        r = self.webhost.request('GET', 'get_eventhub_batch_triggered')
 
-            # Waiting for the blob get updated with the latest data from the
-            # eventhub output binding
-            time.sleep(2)
-            self.assertEqual(r.status_code, 200)
-            entries = r.json()
-            for entry in entries:
-                self.assertEqual(entry['PartitionKey'], partition_key)
-                row_key = entry['RowKey']
-                row_keys_seen[row_key] = True
+        # Waiting for the blob get updated with the latest data from the
+        # eventhub output binding
+        time.sleep(2)
+        self.assertEqual(r.status_code, 200)
+        entries = r.json()
+        for entry in entries:
+            self.assertEqual(entry['PartitionKey'], partition_key)
+            row_key = entry['RowKey']
+            row_keys_seen[row_key] = True
 
-            self.assertDictEqual(all_row_keys_seen, row_keys_seen)
-        finally:
-            self._cleanup(old_partition_key)
+        self.assertDictEqual(all_row_keys_seen, row_keys_seen)
 
     def test_eventhub_multiple_with_metadata(self):
         # Generate a unique event body for EventHub event
@@ -142,36 +133,6 @@ class TestEventHubFunctions(testutils.WebHostTestCase):
             self.assertGreaterEqual(sys_props['SequenceNumber'], 0)
             self.assertIsNotNone(sys_props['Offset'])
 
-    def _cleanup(self, old_partition_key):
-        self._set_table_partition_key(old_partition_key)
-
-    def _get_table_partition_key(self):
-        func_dict = self._get_table_function_json_dict()
-        partition_key = func_dict['bindings'][1]['partitionKey']
-        return partition_key
-
-    def _set_table_partition_key(self, partition_key):
-        full_json_path = self._get_table_function_json_path()
-
-        func_dict = self._get_table_function_json_dict()
-        func_dict['bindings'][1]['partitionKey'] = partition_key
-
-        with open(full_json_path, 'w') as f:
-            json.dump(func_dict, f, indent=2)
-
-    def _get_table_function_json_dict(self):
-        full_json_path = self._get_table_function_json_path()
-
-        with open(full_json_path, 'r') as f:
-            func_dict = json.load(f)
-
-        return func_dict
-
-    def _get_table_function_json_path(self):
-        script_dir = pathlib.Path(self.get_script_dir())
-        json_path = pathlib.Path('get_eventhub_batch_triggered/function.json')
-        return testutils.TESTS_ROOT / script_dir / json_path
-
 
 @skipIf(is_envvar_true(DEDICATED_DOCKER_TEST)
         or is_envvar_true(CONSUMPTION_DOCKER_TEST),
@@ -191,7 +152,7 @@ class TestEventHubBatchFunctionsStein(testutils.WebHostTestCase):
 
     def test_eventhub_multiple(self):
         NUM_EVENTS = 3
-        all_row_keys_seen = dict([(str(i), True) for i in range(NUM_EVENTS)])
+        all_row_keys_seen = dict([(i, True) for i in range(NUM_EVENTS)])
         partition_key = str(round(time.time()))
 
         docs = []
@@ -203,7 +164,7 @@ class TestEventHubBatchFunctionsStein(testutils.WebHostTestCase):
                                  data=json.dumps(docs))
         self.assertEqual(r.status_code, 200)
 
-        row_keys = [str(i) for i in range(NUM_EVENTS)]
+        row_keys = [i for i in range(NUM_EVENTS)]
         seen = [False] * NUM_EVENTS
         row_keys_seen = dict(zip(row_keys, seen))
 
@@ -212,7 +173,7 @@ class TestEventHubBatchFunctionsStein(testutils.WebHostTestCase):
 
         r = self.webhost.request(
             'GET',
-            f'get_eventhub_batch_triggered/{partition_key}')
+            'get_eventhub_batch_triggered')
         self.assertEqual(r.status_code, 200)
         entries = r.json()
         for entry in entries:

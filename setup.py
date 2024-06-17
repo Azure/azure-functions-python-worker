1a21e6318fb4a7c932ed5a9a4528a529a3ebb35e
hallvictoria <victoriahall@microsoft.com>
2024-06-17 13:50:10 -0500

    pin for lc

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ EXTRA_REQUIRES = {
         "scikit-learn",
         "opencv-python",
         "pandas",
-        "numpy~=1.21.6",
+        "numpy~=1.21.0",
         "pre-commit"
     ],
     "test-http-v2": [

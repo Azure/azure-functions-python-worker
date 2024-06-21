3eecd549555f92f4f215de7cd4209c0e0b1c9aca
Victoria Hall <victoria.hall@microsoft.com>
2024-06-21 13:29:46 -0500

    name typo

--- tests/extension_tests/deferred_bindings_tests/deferred_bindings_blob_functions/function_app.py
+++ tests/extension_tests/deferred_bindings_tests/deferred_bindings_blob_functions/function_app.py
@@ -250,13 +250,13 @@ def put_blob_bytes(req: func.HttpRequest, file: func.Out[bytes]) -> str:
 
 
 @app.function_name(name="blob_cache")
-@app.blob_input(arg_name="client",
+@app.blob_input(arg_name="cachedClient",
                 path="python-worker-tests/test-blobclient-triggered.txt",
                 connection="AzureWebJobsStorage")
 @app.route(route="blob_cache")
 def blob_cache(req: func.HttpRequest,
-               cached_client: blob.BlobClient) -> str:
-    return cached_client.download_blob(encoding='utf-8').readall()
+               cachedClient: blob.BlobClient) -> str:
+    return cachedClient.download_blob(encoding='utf-8').readall()
 
 
 @app.function_name(name="invalid_connection_info")

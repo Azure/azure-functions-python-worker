ce2ec9b5d5aaf8dc10b5e493509f0640acbac195
hallvictoria <59299039+hallvictoria@users.noreply.github.com>
2024-07-25 14:56:21 -0500

    fix: use invoke to build protos (#1546)
    * use invoke to build protos
    
    * update to ADO
    
    * correct windows dir
    
    * remove invoke as req dep

--- pack/scripts/mac_arm64_deps.sh
+++ pack/scripts/mac_arm64_deps.sh
@@ -5,6 +5,13 @@ source .env/bin/activate
 python -m pip install --upgrade pip
 
 python -m pip install .
-
 python -m pip install . --no-compile --target "$BUILD_SOURCESDIRECTORY/deps"
+
+python -m pip install invoke
+cd tests
+python -m invoke -c test_setup build-protos
+
+cd ..
 cp .artifactignore "$BUILD_SOURCESDIRECTORY/deps"
+cp azure_functions_worker/protos/FunctionRpc_pb2_grpc.py "$BUILD_SOURCESDIRECTORY/deps/azure_functions_worker/protos"
+cp azure_functions_worker/protos/FunctionRpc_pb2.py "$BUILD_SOURCESDIRECTORY/deps/azure_functions_worker/protos"

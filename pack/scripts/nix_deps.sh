2f59c43a30fd143b861fb10e6e5b48a00a6e1891
hallvictoria <victoriahall@microsoft.com>
2024-07-25 13:12:40 -0500

    use invoke to build protos

--- pack/scripts/nix_deps.sh
+++ pack/scripts/nix_deps.sh
@@ -5,6 +5,10 @@ source .env/bin/activate
 python -m pip install --upgrade pip
 
 python -m pip install .
-
 python -m pip install . --no-compile --target "$BUILD_SOURCESDIRECTORY/deps"
+
+cd tests
+python -m invoke -c test_setup build-protos
+python -m invoke -c test_setup build-protos --no-compile --target "$BUILD_SOURCESDIRECTORY/deps"
+
 cp .artifactignore "$BUILD_SOURCESDIRECTORY/deps"
--- pack/scripts/win_deps.ps1
+++ pack/scripts/win_deps.ps1
@@ -7,4 +7,9 @@ python -m pip install .
 $depsPath = Join-Path -Path $env:BUILD_SOURCESDIRECTORY -ChildPath "deps"
 
 python -m pip install . azure-functions --no-compile --target $depsPath.ToString()
+
+cd tests
+python -m invoke -c test_setup build-protos
+python -m invoke -c test_setup build-protos --no-compile --target "$BUILD_SOURCESDIRECTORY/deps"
+
 Copy-Item -Path ".artifactignore" -Destination $depsPath.ToString()

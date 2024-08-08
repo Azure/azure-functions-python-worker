2f8f424201d905ab1f5d9c5acef158c970e3b00c
hallvictoria <59299039+hallvictoria@users.noreply.github.com>
2024-08-08 14:34:31 -0500

    fix: unique function names (#1557)
    * initial test fixes
    
    * del function_app
    
    * refactor variables into separate file e2e & unit tests
    
    * refactor variables for all pipelines
    
    * typo
    
    * absolute paths
    
    * run all db tests
    
    * typo
    
    ---------
    
    Co-authored-by: Victoria Hall <victoria.hall@microsoft.com>

--- eng/scripts/test-extensions.sh
+++ eng/scripts/test-extensions.sh
@@ -3,7 +3,7 @@
 python -m pip install --upgrade pip
 if [[ $2 != "3.7" ]]; then
     python -m pip install -e $1/PythonExtensionArtifact
-    python -m pip install --pre -U -e .[test-http-v2]
+    python -m pip install --pre -e .[test-http-v2]
 fi
 if [[ $2 != "3.7" && $2 != "3.8" ]]; then
     python -m pip install -e $1/PythonExtensionArtifact
--- eng/scripts/test-sdk.sh
+++ eng/scripts/test-sdk.sh
@@ -2,7 +2,7 @@
 
 python -m pip install --upgrade pip
 python -m pip install -e $1/PythonSdkArtifact
-python -m pip install -U -e .[dev]
+python -m pip install -e .[dev]
 
 if [[ $2 != "3.7" ]]; then
     python -m pip install --pre -U -e .[test-http-v2]

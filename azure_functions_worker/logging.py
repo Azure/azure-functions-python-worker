9546c98ce8f5d2dc2ab6b676b49dde4d44e9f759
peterstone2017 <12449837+YunchuWang@users.noreply.github.com>
2024-09-23 15:48:56 -0700

    feat: add sigterm handler to gracefully shutdown worker and log

--- azure_functions_worker/logging.py
+++ azure_functions_worker/logging.py
@@ -91,6 +91,9 @@ def enable_console_logging() -> None:
     if logger and handler:
         logger.addHandler(handler)
 
+def flush_logger(logger: logging.Logger):
+    for handler in logger.handlers:
+        handler.flush()
 
 def is_system_log_category(ctg: str) -> bool:
     """Check if the logging namespace belongs to system logs. Category starts

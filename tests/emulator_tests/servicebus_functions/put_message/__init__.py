5f6e2a9e5e506417cd8b18be378b5abfbb9ddcdb
Victoria Hall <victoria.hall@microsoft.com>
2024-12-18 10:28:07 -0600

    move servicebus tests to emulators

---
+++ tests/emulator_tests/servicebus_functions/put_message/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+import azure.functions as azf
+
+
+def main(req: azf.HttpRequest, msg: azf.Out[str]):
+    msg.set(req.get_body().decode('utf-8'))
+
+    return 'OK'

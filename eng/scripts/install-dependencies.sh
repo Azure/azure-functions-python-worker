ecf4d99487effdf06e3f54ee733f22386f829b7c
Victoria Hall <victoria.hall@microsoft.com>
2025-01-29 15:49:12 -0600

    working package compilation

--- eng/scripts/install-dependencies.sh
+++ eng/scripts/install-dependencies.sh
@@ -4,9 +4,5 @@ python -m pip install --upgrade pip
 python -m pip install -U azure-functions --pre
 python -m pip install -U -e .[dev]
 
-if [[ $1 != "3.7" ]]; then
-    python -m pip install --pre -U -e .[test-http-v2]
-fi
-if [[ $1 != "3.7" && $1 != "3.8" ]]; then
-    python -m pip install --pre -U -e .[test-deferred-bindings]
-fi
+python -m pip install --pre -U -e .[test-http-v2]
+python -m pip install --pre -U -e .[test-deferred-bindings]

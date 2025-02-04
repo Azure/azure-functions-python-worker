9cc3a67fbd6acce36fd3b537d50edfd87e27046b
Victoria Hall <victoria.hall@microsoft.com>
2025-02-04 09:55:53 -0600

    test extensions fix

--- eng/scripts/test-extensions.sh
+++ eng/scripts/test-extensions.sh
@@ -2,11 +2,11 @@
 
 python -m pip install --upgrade pip
 if [[ $2 != "3.7" ]]; then
-    python -m pip install -e $1/PythonExtensionArtifact
+    python -m pip install -e $1/PythonExtensionArtifact/$3
     python -m pip install --pre -e .[test-http-v2]
 fi
 if [[ $2 != "3.7" && $2 != "3.8" ]]; then
-    python -m pip install -e $1/PythonExtensionArtifact
+    python -m pip install -e $1/PythonExtensionArtifact/$3
     python -m pip install --pre -U -e .[test-deferred-bindings]
 fi
 

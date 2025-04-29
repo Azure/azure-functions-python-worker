2fbf708d3dcd35594c900561701c417d7dbfc213
Evan Roman <evanroman@microsoft.com>
2025-04-29 11:13:55 -0500

    Test

--- tests/emulator_tests/test_eventhub_deferred_bindings.py
+++ tests/emulator_tests/test_eventhub_deferred_bindings.py
@@ -10,12 +10,11 @@ from tests.utils import testutils
 
 @unittest.skipIf(sys.version_info.minor <= 8, "The base extension"
                                               "is only supported for 3.9+.")
-class TestDeferredBindingsEventHubFunctions(testutils.WebHostTestCase):
+class TestEventHubDeferredBindings(testutils.WebHostTestCase):
 
     @classmethod
     def get_script_dir(cls):
-        return testutils.EXTENSION_TESTS_FOLDER / 'deferred_bindings_tests' / \
-            'deferred_bindings_eventhub_functions'
+        return testutils.EMULATOR_TESTS_FOLDER / 'eventhub_functions'
 
     @classmethod
     def get_libraries_to_install(cls):
@@ -34,18 +33,18 @@ class TestDeferredBindingsEventHubFunctions(testutils.WebHostTestCase):
         self.assertEqual(r.status_code, 200)
         self.assertEqual(r.text, 'OK')
 
-        # # Once the event get generated, allow function host to poll from
-        # # EventHub and wait for eventhub_trigger to execute,
-        # # converting the event metadata into a blob.
-        # time.sleep(5)
+        # Once the event get generated, allow function host to poll from
+        # EventHub and wait for eventhub_trigger to execute,
+        # converting the event metadata into a blob.
+        time.sleep(5)
 
-        # # Call get_eh_ed_triggered to retrieve event metadata from blob.
-        # r = self.webhost.request('GET', 'get_eh_ed_triggered')
+        # Call get_eh_ed_triggered to retrieve event metadata from blob.
+        r = self.webhost.request('GET', 'get_eh_ed_triggered')
 
-        # # Waiting for the blob to get updated with the latest data from the
-        # # eventhub output binding
-        # time.sleep(5)
-        # self.assertEqual(r.status_code, 200)
+        # Waiting for the blob to get updated with the latest data from the
+        # eventhub output binding
+        time.sleep(5)
+        self.assertEqual(r.status_code, 200)
 
         # event = r.json()
         # self.assertEqual(event['body'], random_number)

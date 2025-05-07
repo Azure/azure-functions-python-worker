dcf55dbe4f82a597d9cd623945c8738baf6d05c0
Evan Roman <evanroman@microsoft.com>
2025-05-07 16:51:01 -0500

    rm tests

--- tests/emulator_tests/eventhub_functions/eventhub_functions_stein/function_app.py
+++ tests/emulator_tests/eventhub_functions/eventhub_functions_stein/function_app.py
@@ -19,16 +19,6 @@ def eventhub_output(req: func.HttpRequest, event: func.Out[str]):
     event.set(req.get_body().decode('utf-8'))
     return 'OK'
 
-# An HttpTrigger to generating EventHub event from EventHub Output Binding
-@app.function_name(name="eventhub2_output")
-@app.route(route="eventhub2_output")
-@app.event_hub_output(arg_name="event",
-                      event_hub_name="python-worker-ci-eventhub-two",
-                      connection="AzureWebJobsEventHubConnectionString")
-def eventhub2_output(req: func.HttpRequest, event: func.Out[str]):
-    event.set(req.get_body().decode('utf-8'))
-    return 'OK'
-
 
 # This is an actual EventHub trigger which will convert the event data
 # into a storage blob.
@@ -43,20 +33,6 @@ def eventhub2_output(req: func.HttpRequest, event: func.Out[str]):
 def eventhub_trigger(event: func.EventHubEvent) -> bytes:
     return event.get_body()
 
-# This is an actual EventHub trigger which will convert the event data
-# into a storage blob.
-@app.function_name(name="eventhub2_trigger")
-@app.event_hub_message_trigger(arg_name="event",
-                               event_hub_name="python-worker-ci-eventhub-two",
-                               connection="AzureWebJobsEventHubConnectionString"
-                               )
-@app.blob_output(arg_name="$return",
-                 path="python-worker-tests/test-eventhub-triggered2.txt",
-                 connection="AzureWebJobsStorage")
-def eventhub2_trigger(event: func.EventHubEvent) -> bytes:
-    return event.get_body()
-
-
 # Retrieve the event data from storage blob and return it as Http response
 @app.function_name(name="get_eventhub_triggered")
 @app.route(route="get_eventhub_triggered")
@@ -64,17 +40,6 @@ def eventhub2_trigger(event: func.EventHubEvent) -> bytes:
                 path="python-worker-tests/test-eventhub-triggered.txt",
                 connection="AzureWebJobsStorage")
 def get_eventhub_triggered(req: func.HttpRequest,
-                           file: func.InputStream) -> str:
-    return file.read().decode('utf-8')
-
-
-# Retrieve the event data from storage blob and return it as Http response
-@app.function_name(name="get_eventhub2_triggered")
-@app.route(route="get_eventhub2_triggered")
-@app.blob_input(arg_name="file",
-                path="python-worker-tests/test-eventhub-triggered2.txt",
-                connection="AzureWebJobsStorage")
-def get_eventhub2_triggered(req: func.HttpRequest,
                            file: func.InputStream) -> str:
     return file.read().decode('utf-8')
 
--- tests/emulator_tests/test_eventhub_functions.py
+++ tests/emulator_tests/test_eventhub_functions.py
@@ -55,35 +55,6 @@ class TestEventHubFunctions(testutils.WebHostTestCase):
         # Check if the event body matches the initial data
         self.assertEqual(response, doc)
 
-    @testutils.retryable_test(3, 5)
-    def test_eventhub2_trigger(self):
-        # Generate a unique event body for the EventHub event
-        data = str(round(time.time()))
-        doc = {'id': data}
-
-        # Invoke eventhub2_output HttpTrigger to generate an Eventhub Event.
-        r = self.webhost.request('POST', 'eventhub2_output',
-                                 data=json.dumps(doc))
-        self.assertEqual(r.status_code, 200)
-        self.assertEqual(r.text, 'OK')
-
-        # Once the event get generated, allow function host to poll from
-        # EventHub and wait for eventhub_trigger to execute,
-        # converting the event metadata into a blob.
-        time.sleep(5)
-
-        # Call get_eventhub_triggered to retrieve event metadata from blob.
-        r = self.webhost.request('GET', 'get_eventhub2_triggered')
-
-        # Waiting for the blob get updated with the latest data from the
-        # eventhub output binding
-        time.sleep(5)
-        self.assertEqual(r.status_code, 200)
-        response = r.json()
-
-        # Check if the event body matches the initial data
-        self.assertEqual(response, doc)
-
     @skipIf(sys.version_info.minor == 7,
             "Using azure-eventhub SDK with the EventHub Emulator"
             "requires Python 3.8+")
@@ -137,17 +108,17 @@ class TestEventHubFunctions(testutils.WebHostTestCase):
         self.assertIsNotNone(sys_props['Offset'])
 
 
-class TestEventHubFunctionsStein(TestEventHubFunctions):
+# class TestEventHubFunctionsStein(TestEventHubFunctions):
 
-    @classmethod
-    def get_script_dir(cls):
-        return testutils.EMULATOR_TESTS_FOLDER / 'eventhub_functions' / \
-            'eventhub_functions_stein'
+#     @classmethod
+#     def get_script_dir(cls):
+#         return testutils.EMULATOR_TESTS_FOLDER / 'eventhub_functions' / \
+#             'eventhub_functions_stein'
 
 
-class TestEventHubFunctionsSteinGeneric(TestEventHubFunctions):
+# class TestEventHubFunctionsSteinGeneric(TestEventHubFunctions):
 
-    @classmethod
-    def get_script_dir(cls):
-        return testutils.EMULATOR_TESTS_FOLDER / 'eventhub_functions' / \
-            'eventhub_functions_stein' / 'generic'
+#     @classmethod
+#     def get_script_dir(cls):
+#         return testutils.EMULATOR_TESTS_FOLDER / 'eventhub_functions' / \
+#             'eventhub_functions_stein' / 'generic'

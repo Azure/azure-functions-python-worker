43e4b4fd81d08c478634a621f7deb4a23a755527
Victoria Hall <victoriahall@microsoft.com>
2025-05-07 13:12:31 -0500

    Add as part of current sb tests

--- tests/emulator_tests/test_servicebus_functions.py
+++ tests/emulator_tests/test_servicebus_functions.py
@@ -63,3 +63,38 @@ class TestServiceBusFunctionsSteinGeneric(TestServiceBusFunctions):
     def get_script_dir(cls):
         return testutils.EMULATOR_TESTS_FOLDER / 'servicebus_functions' / \
             'servicebus_functions_stein' / 'generic'
+
+
+class TestServiceBusSDKFunctions(testutils.WebHostTestCase):
+
+    @classmethod
+    def get_script_dir(cls):
+        return testutils.EMULATOR_TESTS_FOLDER / 'servicebus_sdk_functions'
+
+    @testutils.retryable_test(3, 5)
+    def test_servicebus_basic(self):
+        data = str(round(time.time()))
+        r = self.webhost.request('POST', 'put_message',
+                                 data=data)
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.text, 'OK')
+
+        max_retries = 10
+
+        for try_no in range(max_retries):
+            # wait for trigger to process the queue item
+            time.sleep(1)
+
+            try:
+                r = self.webhost.request('GET', 'get_servicebus_triggered')
+                self.assertEqual(r.status_code, 200)
+                msg = r.json()
+                self.assertEqual(msg['body'], data)
+                for attr in {'message', 'body', 'enqueued_time_utc', 'lock_token',
+                             'locked_until', 'message_id', 'sequence_number'}:
+                    self.assertIn(attr, msg)
+            except (AssertionError, json.JSONDecodeError):
+                if try_no == max_retries - 1:
+                    raise
+            else:
+                break

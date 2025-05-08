3078d4d2f9d1f24d88eb70802e6ae14cec816599
Victoria Hall <victoriahall@microsoft.com>
2025-05-08 09:53:47 -0500

    directory fix

--- tests/emulator_tests/test_deferred_bindings_blob_functions.py
+++ tests/emulator_tests/test_deferred_bindings_blob_functions.py
@@ -13,7 +13,7 @@ class TestDeferredBindingsBlobFunctions(testutils.WebHostTestCase):
 
     @classmethod
     def get_script_dir(cls):
-        return testutils.E2E_TESTS_FOLDER / 'blob_functions' / \
+        return testutils.EMULATOR_TESTS_FOLDER / 'blob_functions' / \
             'blob_functions_sdk'
 
     @classmethod

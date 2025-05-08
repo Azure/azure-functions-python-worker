a6b1d0d6d139feb3ccea5275f76eb1ee5286587d
Victoria Hall <victoriahall@microsoft.com>
2025-05-08 10:41:24 -0500

    directory fix

--- tests/unittests/test_deferred_bindings.py
+++ tests/unittests/test_deferred_bindings.py
@@ -19,15 +19,12 @@ if sys.version_info.minor >= 9:
                                                          StorageStreamDownloader)
 
 DEFERRED_BINDINGS_ENABLED_DIR = testutils.UNIT_TESTS_FOLDER / \
-    'deferred_bindings_tests' / \
     'deferred_bindings_functions' / \
     'deferred_bindings_enabled'
 DEFERRED_BINDINGS_DISABLED_DIR = testutils.UNIT_TESTS_FOLDER / \
-    'deferred_bindings_tests' / \
     'deferred_bindings_functions' / \
     'deferred_bindings_disabled'
 DEFERRED_BINDINGS_ENABLED_DUAL_DIR = testutils.UNIT_TESTS_FOLDER / \
-    'deferred_bindings_tests' / \
     'deferred_bindings_functions' / \
     'deferred_bindings_enabled_dual'
 

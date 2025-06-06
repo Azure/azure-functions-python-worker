73e2e676213e0f5637c3f0d03d2736b6b24ecf09
Evan Roman <evanroman@microsoft.com>
2025-06-06 10:43:49 -0500

    chng to linux

--- azure_functions_worker/main.py
+++ azure_functions_worker/main.py
@@ -3,6 +3,7 @@
 """Main entrypoint."""
 
 import argparse
+import uvloop
 
 
 def parse_args():
@@ -46,7 +47,7 @@ def main():
     DependencyManager.use_worker_dependencies()
 
     import asyncio
-    # asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
     from . import logging
     from .logging import error_logger, format_exception, logger

fb5b8c9c04d9f93bd1370660fc1df9057bb52cba
Victoria Hall <victoriahall@microsoft.com>
2025-06-23 14:01:05 -0500

    remove only binary

--- eng/scripts/install-dependencies.sh
+++ eng/scripts/install-dependencies.sh
@@ -12,4 +12,4 @@ if [[ $1 != "3.7" && $1 != "3.8" ]]; then
     python -m pip install --pre -U -e .[test-deferred-bindings]
 fi
 
-pip install grpcio==1.35.0 protobuf==3.9.0 --only-binary=:all: -t tests/endtoend/dependency_isolation_functions/.python_packages_grpc_protobuf/lib/site-packages
+pip install grpcio==1.35.0 protobuf==3.9.0 -t tests/endtoend/dependency_isolation_functions/.python_packages_grpc_protobuf/lib/site-packages

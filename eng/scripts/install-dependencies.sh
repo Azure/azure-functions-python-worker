d300cd540f0c44b4026ac41d5f532ba349077f82
Victoria Hall <victoriahall@microsoft.com>
2025-06-25 16:04:12 -0500

    fix working dir

--- eng/scripts/install-dependencies.sh
+++ eng/scripts/install-dependencies.sh
@@ -2,11 +2,11 @@
 
 python -m pip install --upgrade pip
 python -m pip install -U azure-functions --pre
-python -m pip install -U -e .[dev]
+python -m pip install -U -e workers/[dev]
 
 if [[ $1 != "3.7" ]]; then
-    python -m pip install --pre -U -e .[test-http-v2]
+    python -m pip install --pre -U -e workers/[test-http-v2]
 fi
 if [[ $1 != "3.7" && $1 != "3.8" ]]; then
-    python -m pip install --pre -U -e .[test-deferred-bindings]
+    python -m pip install --pre -U -e workers/[test-deferred-bindings]
 fi

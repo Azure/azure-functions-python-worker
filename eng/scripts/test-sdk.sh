71be65458fba10b211e74cb84aec246f0aec8609
Victoria Hall <victoriahall@microsoft.com>
2025-07-01 10:28:44 -0500

    fix parameter passing

--- eng/scripts/test-sdk.sh
+++ eng/scripts/test-sdk.sh
@@ -3,11 +3,11 @@
 cd workers
 python -m pip install --upgrade pip
 python -m pip install -e $1/PythonSdkArtifact
-python -m pip install -e .[dev]
+python -m pip install -e workers/[dev]
 
 if [[ $2 != "3.7" ]]; then
-    python -m pip install --pre -U -e .[test-http-v2]
+    python -m pip install --pre -U -e workers/[test-http-v2]
 fi
 if [[ $2 != "3.7" && $2 != "3.8" ]]; then
-    python -m pip install --pre -U -e .[test-deferred-bindings]
+    python -m pip install --pre -U -e workers/[test-deferred-bindings]
 fi

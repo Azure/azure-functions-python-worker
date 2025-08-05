3baf9cb8437f1966507b53d6c44d899df6c4b180
Evan Roman <evanroman@microsoft.com>
2025-08-05 13:08:43 -0500

    fix

--- workers/tests/unittests/test_logging.py
+++ workers/tests/unittests/test_logging.py
@@ -57,4 +57,4 @@ class TestLogging(unittest.TestCase):
             self.assertIn("call2", processed_exception)
             self.assertIn("f", processed_exception)
             self.assertRegex(processed_exception,
-                             r".*tests\\unittests\\test_logging.py.*")
+                             r".*tests/unittests/test_logging.py.*")

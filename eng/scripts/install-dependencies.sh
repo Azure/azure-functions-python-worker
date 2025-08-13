f910d1f76c657f07595760f13a32d65294bcc575
Victoria Hall <victoriahall@microsoft.com>
2025-08-13 14:21:02 -0500

    stop uninstalling

--- eng/scripts/install-dependencies.sh
+++ eng/scripts/install-dependencies.sh
@@ -3,8 +3,6 @@
 python -m pip install --upgrade pip
 python -m pip install -U azure-functions --pre
 python -m pip install -U -e $2/[dev]
-python -m pip install -e runtimes/v2
-python -m pip install -e runtimes/v1
 
 if [[ $1 != "3.7" ]]; then
     python -m pip install --pre -U -e $2/[test-http-v2]

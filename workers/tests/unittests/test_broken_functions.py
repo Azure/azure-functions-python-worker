81e5e78d49c666386b58d8c59334cc408832f4be
Victoria Hall <victoriahall@microsoft.com>
2025-08-20 13:59:06 -0500

    explicit fail for invalid streaming case + lint

--- workers/tests/unittests/test_broken_functions.py
+++ workers/tests/unittests/test_broken_functions.py
@@ -21,7 +21,7 @@ class TestMockHost(testutils.AsyncTestCase):
             self.assertRegex(
                 r.response.result.exception.message,
                 r".*cannot load the missing_py_param function"
-                r".*parameters are declared in function.json"
+                r".*parameters are declared in the function definition"
                 r".*'req'.*")
 
     async def test_load_broken__missing_json_param(self):
@@ -218,7 +218,8 @@ class TestMockHost(testutils.AsyncTestCase):
             self.assertEqual(
                 r.response.result.exception.message,
                 'FunctionLoadError: cannot load the invalid_http_trigger_anno'
-                ' function: type of req binding in function.json "httpTrigger" '
+                ' function: type of req binding in function definition '
+                '(function.json or function decorators) "httpTrigger" '
                 'does not match its Python annotation "int"')
 
     async def test_load_broken__invalid_out_anno(self):
@@ -234,7 +235,8 @@ class TestMockHost(testutils.AsyncTestCase):
             self.assertEqual(
                 r.response.result.exception.message,
                 'FunctionLoadError: cannot load the invalid_out_anno function: '
-                r'type of ret binding in function.json "http" '
+                r'type of ret binding in function definition '
+                r'(function.json or function decorators) "http" '
                 r'does not match its Python annotation "HttpRequest"')
 
     async def test_load_broken__invalid_in_anno(self):
@@ -250,7 +252,8 @@ class TestMockHost(testutils.AsyncTestCase):
             self.assertEqual(
                 r.response.result.exception.message,
                 'FunctionLoadError: cannot load the invalid_in_anno function: '
-                r'type of req binding in function.json "httpTrigger" '
+                r'type of ret binding in function definition '
+                r'(function.json or function decorators) "httpTrigger" '
                 r'does not match its Python annotation "HttpResponse"')
 
     async def test_load_broken__invalid_datatype(self):
@@ -267,7 +270,8 @@ class TestMockHost(testutils.AsyncTestCase):
                 r.response.result.exception.message,
                 r'.*cannot load the invalid_datatype function: '
                 r'.*binding type "httpTrigger" and dataType "1" in '
-                r'function.json do not match the corresponding function '
+                r'function definition (function.json or function decorators) '
+                r'do not match the corresponding function '
                 r'parameter.* Python type annotation "HttpResponse"')
 
     async def test_load_broken__invalid_in_anno_non_type(self):

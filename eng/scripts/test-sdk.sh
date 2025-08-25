6e68b891d1f9780b8e9d2cfb7089d873a905578a
Gavin Aguiar <gaaguiar@microsoft.com>
2025-08-25 16:05:17 -0500

    Added unit tests

--- eng/scripts/test-sdk.sh
+++ eng/scripts/test-sdk.sh
@@ -2,12 +2,13 @@
 
 cd workers
 python -m pip install --upgrade pip
-python -m pip install -e $1/PythonSdkArtifact
-python -m pip install -e workers/[dev]
+python -m pip install uv
+python -m uv pip install -e $1/PythonSdkArtifact
+python -m uv pip install -e workers/[dev]
 
 if [[ $2 != "3.7" ]]; then
-    python -m pip install --pre -U -e workers/[test-http-v2]
+    python -m uv pip install --pre -U -e workers/[test-http-v2]
 fi
 if [[ $2 != "3.7" && $2 != "3.8" ]]; then
-    python -m pip install --pre -U -e workers/[test-deferred-bindings]
+    python -m uv pip install --pre -U -e workers/[test-deferred-bindings]
 fi

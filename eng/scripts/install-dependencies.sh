e5c430d53bfa0aa39e3a882379397183111986fb
Gavin Aguiar <gaaguiar@microsoft.com>
2025-08-25 16:16:06 -0500

    Reverting uv change

--- eng/scripts/install-dependencies.sh
+++ eng/scripts/install-dependencies.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 python -m pip install --upgrade pip
-python -m pip install uv
-python -m uv pip install -e runtimes/v2
-python -m uv pip install -e runtimes/v1
-python -m uv pip install -U azure-functions --pre
-python -m uv pip install -U -e $2/[dev]
+python -m pip install -e runtimes/v2
+python -m pip install -e runtimes/v1
+python -m pip install -U azure-functions --pre
+python -m pip install -U -e $2/[dev]
 
 if [[ $1 != "3.7" ]]; then
-    python -m uv pip install --pre -U -e $2/[test-http-v2]
+    python -m pip install --pre -U -e $2/[test-http-v2]
 fi
 if [[ $1 != "3.7" && $1 != "3.8" ]]; then
-    python -m uvpip install --pre -U -e $2/[test-deferred-bindings]
+    python -m pip install --pre -U -e $2/[test-deferred-bindings]
 fi
--- eng/scripts/test-extensions.sh
+++ eng/scripts/test-extensions.sh
@@ -2,14 +2,13 @@
 
 cd workers
 python -m pip install --upgrade pip
-python -m pip install uv
 if [[ $2 != "3.7" ]]; then
-    python -m uv pip install -e $1/PythonExtensionArtifact/$3
-    python -m uv pip install --pre -e workers/[test-http-v2]
+    python -m pip install -e $1/PythonExtensionArtifact/$3
+    python -m pip install --pre -e workers/[test-http-v2]
 fi
 if [[ $2 != "3.7" && $2 != "3.8" ]]; then
-    python -m uv pip install -e $1/PythonExtensionArtifact/$3
-    python -m uv pip install --pre -U -e workers/[test-deferred-bindings]
+    python -m pip install -e $1/PythonExtensionArtifact/$3
+    python -m pip install --pre -U -e workers/[test-deferred-bindings]
 fi
 
-python -m uv pip install -U -e workers/[dev]
+python -m pip install -U -e workers/[dev]

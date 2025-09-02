df51709c146d508fd007874c92b674c66439f598
Victoria Hall <victoriahall@microsoft.com>
2025-09-02 11:22:12 -0500

    correct params

--- eng/scripts/install-dependencies.sh
+++ eng/scripts/install-dependencies.sh
@@ -6,13 +6,13 @@ python -m pip install -e runtimes/v1
 python -m pip install -U azure-functions --pre
 
 # uv is only available on Python 3.8+
-if [[ $2 != "3.7" ]]; then
+if [[ $1 != "3.7" ]]; then
     python -m pip install uv
     python -m uv pip install -U -e $2/[dev]
 fi
 
 # Install normal way for 3.7
-if [[ $2 == "3.7" ]]; then
+if [[ $1 == "3.7" ]]; then
     python -m pip install -U -e $2/[dev]
 fi
 

6e3b7f3e4a3206ff4e7b9fcd442813c85e28095b
Victoria Hall <victoriahall@microsoft.com>
2025-09-11 11:57:34 -0500

    fix test dir pt 2

--- workers/tests/endtoend/test_http_v2.py
+++ workers/tests/endtoend/test_http_v2.py
@@ -222,7 +222,7 @@ class TestHttpFunctionsWithInitIndexingDisabled(testutils.WebHostTestCase):
 
     @classmethod
     def get_script_dir(cls):
-        return testutils.E2E_TESTS_FOLDER / 'http_v2_tests' / \
+        return testutils.E2E_TESTS_FOLDER / \
             'http_functions_v2' / \
             'fastapi'
 

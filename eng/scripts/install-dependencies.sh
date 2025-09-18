5cb51cabc9aee84ed7cc841289985bbbed06a2ea
Victoria Hall <victoriahall@microsoft.com>
2025-09-18 16:40:10 -0500

    there is potential

--- eng/scripts/install-dependencies.sh
+++ eng/scripts/install-dependencies.sh
@@ -10,8 +10,8 @@ if [[ $1 != "3.7" ]]; then
     python -m pip install --pre -U -e $2/[test-http-v2]
 fi
 if [[ $1 != "3.7" && $1 != "3.8" ]]; then
-    python -m pip install --pre -U --no-deps azurefunctions-extensions-bindings-blob==1.1.1
-    python -m pip install --pre -U --no-deps azurefunctions-extensions-bindings-eventhub==1.0.0b1
+    python -m pip install --pre -U azurefunctions-extensions-bindings-blob==1.1.1
+    python -m pip install --pre -U azurefunctions-extensions-bindings-eventhub==1.0.0b1
     python -m pip install --pre -U --no-deps azurefunctions-extensions-bindings-servicebus==1.0.0b2
 
 

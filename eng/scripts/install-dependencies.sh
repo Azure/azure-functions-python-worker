36deb68e587a511716fa68a6dfd955c5882b49de
Victoria Hall <victoriahall@microsoft.com>
2025-09-18 16:04:28 -0500

    no deps

--- eng/scripts/install-dependencies.sh
+++ eng/scripts/install-dependencies.sh
@@ -10,7 +10,5 @@ if [[ $1 != "3.7" ]]; then
     python -m pip install --pre -U -e $2/[test-http-v2]
 fi
 if [[ $1 != "3.7" && $1 != "3.8" ]]; then
-    INSTALL_DIR="./extensions_dir"
-    python -m pip install --pre -U --target "$INSTALL_DIR" "$2/[test-deferred-bindings]"
-    python -c "import sys; sys.path.insert(0, '$INSTALL_DIR'); import azurefunctions.extensions.bindings.blob as blob; print(blob.__version__)"
+    python -m pip install --pre -U --no-deps "$2/[test-deferred-bindings]"
 fi

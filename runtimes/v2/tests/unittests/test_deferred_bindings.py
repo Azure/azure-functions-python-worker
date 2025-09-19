689779948dce138679e440b38f8c0f4279eac2a7
Victoria Hall <victoriahall@microsoft.com>
2025-09-19 11:59:45 -0500

    fix test

--- runtimes/v2/tests/unittests/test_deferred_bindings.py
+++ runtimes/v2/tests/unittests/test_deferred_bindings.py
@@ -16,7 +16,7 @@ from azurefunctions.extensions.bindings.blob import (BlobClient,
                                                      ContainerClient,
                                                      StorageStreamDownloader)
 from azurefunctions.extensions.bindings.eventhub import EventData, EventDataConverter
-from azurefunctions.extensions.bindings.servicebus import ServiceBusMessageActions
+from azurefunctions.extensions.base import GrpcClientType
 
 EVENTHUB_SAMPLE_CONTENT = b"\x00Sr\xc1\x8e\x08\xa3\x1bx-opt-sequence-number-epochT\xff\xa3\x15x-opt-sequence-numberU\x04\xa3\x0cx-opt-offset\x81\x00\x00\x00\x01\x00\x00\x010\xa3\x13x-opt-enqueued-time\x00\xa3\x1dcom.microsoft:datetime-offset\x81\x08\xddW\x05\xc3Q\xcf\x10\x00St\xc1I\x02\xa1\rDiagnostic-Id\xa1700-bdc3fde4889b4e907e0c9dcb46ff8d92-21f637af293ef13b-00\x00Su\xa0\x08message1"  # noqa: E501
 
@@ -106,7 +106,7 @@ class TestDeferredBindingsEnabled(testutils.AsyncTestCase):
         annotations = {
             'param1': func.InputStream,
             'param2': func.Out[str],
-            'param3': ServiceBusMessageActions
+            'param3': GrpcClientType
         }
 
         settlement_client_arg = meta.validate_settlement_param(
@@ -134,7 +134,7 @@ class TestDeferredBindingsEnabled(testutils.AsyncTestCase):
         annotations = {
             'param1': func.InputStream,
             'param2': func.Out[str],
-            'param3': ServiceBusMessageActions,
+            'param3': GrpcClientType,
             'param4': str
         }
 

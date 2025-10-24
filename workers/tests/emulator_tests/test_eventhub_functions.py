8cfdbc85e2f75e5a4e100fcbffafe769817b78f9
Victoria Hall <victoriahall@microsoft.com>
2025-10-24 10:13:07 -0500

    skip eventhub tests for 314

--- workers/tests/emulator_tests/test_eventhub_functions.py
+++ workers/tests/emulator_tests/test_eventhub_functions.py
@@ -124,6 +124,7 @@ class TestEventHubFunctionsSteinGeneric(TestEventHubFunctions):
             'eventhub_functions_stein' / 'generic'
 
 
+@skipIf(sys.version_info.minor >= 14, "Skip to figure out uamqp.")
 class TestEventHubFunctionsSDK(TestEventHubFunctions):
 
     @classmethod

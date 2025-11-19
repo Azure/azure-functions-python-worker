cb27727afe63085e8db93622125fd5c90ffd64dc
Gavin Aguiar <gaaguiar@microsoft.com>
2025-11-19 16:27:25 -0600

    Removed unused imports

--- workers/tests/emulator_tests/test_blob_functions.py
+++ workers/tests/emulator_tests/test_blob_functions.py
@@ -1,11 +1,9 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-import sys
 import time
 
 from requests import JSONDecodeError
 from tests.utils import testutils
-from unittest.case import skipIf
 
 
 class TestBlobFunctions(testutils.WebHostTestCase):

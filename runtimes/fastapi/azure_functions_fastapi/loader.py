0815188a52fae9f5de638ad47cad1e70cdcf05cf
Victoria Hall <victoriahall@microsoft.com>
2026-08-26 15:52:42 -0500

    Feedback

--- runtimes/fastapi/azure_functions_fastapi/loader.py
+++ runtimes/fastapi/azure_functions_fastapi/loader.py
@@ -226,10 +226,9 @@ def load_function_metadata(function_path: str, function_dir: str, protos) -> Tup
         - fastapi_app: The FastAPI application instance
         - metadata_results: List of RpcFunctionMetadata protobuf objects
     """
-    # Add current directory to Python path if needed
-    current_dir = os.getcwd()
-    if current_dir not in sys.path:
-        sys.path.insert(0, current_dir)
+    function_app_directory = os.path.dirname(os.path.abspath(function_path))
+    if function_app_directory not in sys.path:
+        sys.path.insert(0, function_app_directory)
     
     logger.info(f"Indexing FastAPI app from {function_path}")
     
